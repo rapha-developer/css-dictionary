8df23277396cdb475483c40adaf12fb995a55c73
rapha-desenvolvedor <contatosraphaelmoreira@gmail.com>
2022-12-18 20:22:54 -0300

    Import and use component called Styleguide inside App Component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import Styleguide from './pages/styleguide/Styleguide'
 
 function App() {
 
   return (
     <div className="App">
-      <h1 className="">Hello world</h1>
+      <Styleguide />
     </div>
   )
 }
