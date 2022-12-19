96a2ecc769b870c6e86cac124275fd2da0393748
rapha-desenvolvedor <contatosraphaelmoreira@gmail.com>
2022-12-19 16:48:53 -0300

    Import and use component called StylePage inside component App

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
-import Styleguide from './pages/styleguide/Styleguide'
+import StylePage from './pages/styleguide/StylePage'
 
 function App() {
 
   return (
     <div className="App">
-      <Styleguide />
+      <StylePage />
     </div>
   )
 }
