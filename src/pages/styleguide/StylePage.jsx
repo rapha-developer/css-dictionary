a85c8ce26e01f1868327046df76f2f6353316734
rapha-desenvolvedor <contatosraphaelmoreira@gmail.com>
2022-12-20 18:24:13 -0300

    Import and use StyleColorPage and StyleTypographyPage inside the StylePage component.

--- src/pages/styleguide/StylePage.jsx
+++ src/pages/styleguide/StylePage.jsx
@@ -2,35 +2,13 @@
 import StyleColor from './components/colors/StyleColor'
 import StyleHeader from './components/header/StyleHeader'
 import { createStyleHeader, createStyleColor } from '../../data/constants'
+import StyleColorPage from './pages/colors/StyleColorPage'
+import StyleTypographyPage from './pages/typography/StyleTypographyPage'
 function StylePage() {
     return (
         <div className="stylePage">
-            <StyleHeader 
-                text__primary={createStyleHeader.colors.text__primary}
-                text__secondary={createStyleHeader.colors.text__secondary}
-                text__tertiary={createStyleHeader.colors.text__tertiary}
-            />
-            <StyleColor 
-                text__primary={createStyleColor.primary.text__primary}
-                text__secondary={createStyleColor.primary.text__secondary}
-                palette={createStyleColor.primary.palette}
-            />
-            <StyleColor 
-                text__primary={createStyleColor.secondary.text__primary}
-                text__secondary={createStyleColor.secondary.text__secondary}
-                palette={createStyleColor.secondary.palette}
-            />
-            <StyleColor 
-                text__primary={createStyleColor.typography.text__primary}
-                text__secondary={createStyleColor.typography.text__secondary}
-                palette={createStyleColor.typography.palette}
-            />
-            <StyleColor 
-                text__primary={createStyleColor.background.text__primary}
-                text__secondary={createStyleColor.background.text__secondary}
-                palette={createStyleColor.background.palette}
-            />
-        </div>
+            <StyleColorPage />
+        </div> 
     )
 }
 export default StylePage
