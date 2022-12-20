0741574f66bda06f465b896ebf1e37f884221ee8
rapha-desenvolvedor <contatosraphaelmoreira@gmail.com>
2022-12-20 10:34:17 -0300

    Create and export two new constants: createStyleHeader and createStyleColor

---
+++ src/data/constants.js
@@ -0,0 +1,121 @@
+const createStyleHeader = {
+    colors: {
+        text__secondary: "style guide",
+        text__primary: "color styles",
+        text__tertiary: "Welcome to the style guide. This page is the focal point for stylistic changes to this product. If you have any questions regarding this product , write to us at our mail hello@unisense.agency"
+    },
+    typography: {},
+    boxShadows: {},
+    elements: {}
+
+}
+const createStyleColor = {
+    primary: {
+        text__primary: "primary colors",
+        text__secondary: "These are the main colors of the product that are used in various Ui elements.",
+        palette: [
+            {
+                name: "primary 1",
+                rgb: "#5B7FFF"
+            },
+            {
+                name: "primary 2",
+                rgb: "#38CB89"
+            },
+            {
+                name: "primary 3",
+                rgb: "#FFAB00"
+            },
+            {
+                name: "primary 4",
+                rgb: "#FF5630"
+            }
+        ]
+    },
+    secondary: {
+        text__primary: "secondary colors",
+        text__secondary: "These are the secondary colors of the product that are used in various Ui elements.",
+        palette: [
+            {
+                name: "secondary 1",
+                rgb: "#E1ECFF"
+            },
+            {
+                name: "secondary 2",
+                rgb: "#E5F6EF"
+            },
+            {
+                name: "secondary 3",
+                rgb: "#FDF4E0"
+            },
+            {
+                name: "secondary 4",
+                rgb: "#FFE6E0"
+            },
+            {
+                name: "transparent 1",
+                rgb: "#F4F8FF"
+            },
+            {
+                name: "transparent 2",
+                rgb: "#F5FCF9"
+            },
+            {
+                name: "transparent 3",
+                rgb: "#FFFBF1"
+            },
+            {
+                name: "transparent 4",
+                rgb: "#FFF6F4"
+            },
+        ]
+    },
+    typography: {
+        text__primary: "typography colors",
+        text__secondary: "These are the colors that are used in the font base of the product.",
+        palette: [
+            {
+                name: "Text 1",
+                rgb: "#323B4B"
+            },
+            {
+                name: "Text 2",
+                rgb: "#4E5D78"
+            },
+            {
+                name: "Text 3",
+                rgb: "#8A94A6"
+            },
+            {
+                name: "Text 4",
+                rgb: "#B0B7C3"
+            },
+        ]
+    },
+    background: {
+        text__primary: "background colors",
+        text__secondary: "These are the colors that are used to color the background in various products layouts.",
+        palette: [
+            {
+                name: "BG 1",
+                rgb: "#C1C7D0"
+            },
+            {
+                name: "BG 2",
+                rgb: "#EBECF0"
+            },
+            {
+                name: "BG 3",
+                rgb: "#FAFBFC"
+            },
+            {
+                name: "BG 4",
+                rgb: "#FFFFFF"
+            },
+        ]
+    }
+}
+export {
+    createStyleHeader,
+    createStyleColor
+}
