e74d430190143da88bf123249d4cc7408ce5fe4f
rapha-desenvolvedor <contatosraphaelmoreira@gmail.com>
2022-12-20 10:53:50 -0300

    Update StyleguideHeader to StyleHeader component. Sets text_primary, text__secondary and text__tertiary. And display them in their respectives places.

--- src/pages/styleguide/components/header/StyleHeader.jsx
+++ src/pages/styleguide/components/header/StyleHeader.jsx
@@ -1,25 +1,21 @@
 
 import styles from './style.module.css'
-function StyleguideHeader(props) {
-    const headerData = {
-        subtitle: "style guide",
-        title: "color styles",
-        description: "Welcome to the style guide. This page is the focal point for stylistic changes to this product. If you have any questions regarding this product , write to us at our mail hello@unisense.agency"
-    }
+function StyleHeader({text__primary, text__secondary, text__tertiary}) {
+
     return (
         <div className={`${styles.header} bg-primary-300`}>
             <div className="container">
                 <h5 className={`${styles.text__secondary} ff-inter fs-small-100 fw-semi-bold text-color-400 uppercase letter-spacing-1`}>
-                    {headerData.subtitle}
+                    {text__secondary}
                 </h5>
                 <h1 className={`${styles.text__primary} ff-inter fs-h200 fw-bold text-color-100 capitalize`}>
-                    {headerData.title}
+                    {text__primary}
                 </h1>
                 <p className={`${styles.text__tertiary} ff-inter fs-body-200 fw-medium text-color-400 line-height-35`}>
-                    {headerData.description}
+                    {text__tertiary}
                 </p>
             </div>
         </div>
     )
 }
-export default StyleguideHeader
+export default StyleHeader
