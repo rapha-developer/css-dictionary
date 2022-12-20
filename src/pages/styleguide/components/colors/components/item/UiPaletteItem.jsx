88dd3d6b806c59942fafa62a01157e428c63da76
rapha-desenvolvedor <contatosraphaelmoreira@gmail.com>
2022-12-20 10:37:40 -0300

    Update class .fs-h900 to fs-body-200

--- src/pages/styleguide/components/colors/components/item/UiPaletteItem.jsx
+++ src/pages/styleguide/components/colors/components/item/UiPaletteItem.jsx
@@ -5,7 +5,7 @@ function UiPaletteItem({colorName, rgbColor}) {
         <div className={`${styles.palette__item} box-shadow-1`}>
             <div className={styles.palette__head} style={{background: rgbColor}}></div>
             <div className={`${styles.palette__body} bg-primary-400`}>
-                <p className="palette__name ff-inter fs-h900 fw-semi-bold text-color-100 capitalize">
+                <p className="palette__name ff-inter fs-body-200 fw-semi-bold text-color-100 capitalize">
                     {colorName}
                 </p>
                 <p className="palette__rgb ff-inter fs-small-100 fw-semi-bold text-color-400">{rgbColor}</p>
