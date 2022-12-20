d45703386416b2cc02314112027d35901f859ae7
rapha-desenvolvedor <contatosraphaelmoreira@gmail.com>
2022-12-20 10:40:41 -0300

    Remove unnecessary constant. Define palette as props of the component. Use map function from palette props.

--- src/pages/styleguide/components/colors/components/palette/UiPaletteCollection.jsx
+++ src/pages/styleguide/components/colors/components/palette/UiPaletteCollection.jsx
@@ -1,26 +1,9 @@
 
 import UiPaletteItem from '../item/UiPaletteItem';
 import styles from './style.module.css'
-function UiPaletteCollection() {
-    const palette__colors = [
-        {
-            name: "primary 1",
-            rgb: "#5B7FFF"
-        },
-        {
-            name: "primary 2",
-            rgb: "#38CB89"
-        },
-        {
-            name: "primary 3",
-            rgb: "#FFAB00"
-        },
-        {
-            name: "primary 4",
-            rgb: "#FF5630"
-        }
-    ];
-    const palette__collection = palette__colors.map((paletteItem) => {
+function UiPaletteCollection({palette}) {
+
+    const palette__collection = palette.map((paletteItem) => {
         return (
             <UiPaletteItem 
                 key={paletteItem.rgb}
