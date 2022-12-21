c23bd570bebc7e6a751e8bbda93a6ecc7a235015
rapha-desenvolvedor <contatosraphaelmoreira@gmail.com>
2022-12-21 11:35:55 -0300

    Import and use StyleTypography component inside StyleTypographyPage

--- src/pages/styleguide/pages/typography/StyleTypographyPage.jsx
+++ src/pages/styleguide/pages/typography/StyleTypographyPage.jsx
@@ -1,6 +1,7 @@
 
 import StyleHeader from "../../components/header/StyleHeader"
 import { createStyleHeader } from '../../../../data/constants'
+import StyleTypography from "../../components/typography/StyleTypography"
 function StyleTypographyPage() {
     return (
         <div className="styleTypographyPage">
@@ -9,6 +10,7 @@ function StyleTypographyPage() {
                 text__primary={createStyleHeader.typography.text__primary}
                 text__tertiary={createStyleHeader.typography.text__tertiary}
             />
+            <StyleTypography />
         </div>
     )
 }
