0986faf6075e71ea82e86aa05e5fc4ca72a902be
rapha-desenvolvedor <contatosraphaelmoreira@gmail.com>
2023-03-22 19:05:07 -0300

    Import UiAccordion and UiSidebarGroup component(s) and place inside UiSidebar with your data (createFirstAccordionData, createFirstSidebarGroupData). And styles needed.

--- src/components/sidebar/UiSidebar.jsx
+++ src/components/sidebar/UiSidebar.jsx
@@ -1,6 +1,8 @@
-
 import boxboardLogo from '../../assets/boxboard-logo.svg'
 import styles from './style.module.css'
+import UiAccordion from '../ui/accordion/UiAccordion'
+import { createFirstAccordionData, createFirstSidebarGroupData } from '../../data/constants'
+import UiSidebarGroup from './group/UiSidebarGroup'
 function UiSidebar() {
     return (
         <div className={styles.sidebar}>
@@ -9,8 +11,13 @@ function UiSidebar() {
                 alt="Boxboard logo" 
                 className={styles.sidebar__logo} />
             </div>
-            <div className="sidebar__body">
-
+            <div className={styles.sidebar__body}>
+                <UiSidebarGroup 
+                    items={createFirstSidebarGroupData}
+                />
+                <UiAccordion 
+                    items={createFirstAccordionData}
+                />
             </div>
         </div>
     )
