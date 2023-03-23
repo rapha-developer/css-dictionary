e0c59d62f151273259a99c09216582a38bde2a06
rapha-desenvolvedor <contatosraphaelmoreira@gmail.com>
2023-03-23 11:37:28 -0300

    Create the format property (in props) and display it in the component

--- src/components/ui/accordion/item/UiAccordionItem.jsx
+++ src/components/ui/accordion/item/UiAccordionItem.jsx
@@ -3,7 +3,7 @@ import styles from './style.module.css'
 function UiAccordionItem(props) {
     return (
         <li className={styles.accordionMenuItem}>
-            <a href={props.url} className={`${styles.accordionMenuLink} capitalize`}><VscCircle className={styles.accordionCircleIcon} strokeWidth="1" data-color={props.iconColor} /> {props.name}</a>
+            <a href={props.url} className={`${styles.accordionMenuLink} ${props.format}`}><VscCircle className={styles.accordionCircleIcon} strokeWidth="1" data-color={props.iconColor} /> {props.name}</a>
         </li>
     )
 }
