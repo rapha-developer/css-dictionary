22e8d31724108efa2cd14de170e91cff9a32faea
rapha-desenvolvedor <contatosraphaelmoreira@gmail.com>
2023-03-23 11:25:04 -0300

    Refactor iconType decision to a function return

--- src/components/sidebar/group/item/UiSidebarGroupItem.jsx
+++ src/components/sidebar/group/item/UiSidebarGroupItem.jsx
@@ -2,8 +2,10 @@ import { AiTwotoneHeart } from 'react-icons/ai'
 import { BsFillClockFill } from 'react-icons/bs'
 import styles from './style.module.css'
 function UiSidebarGroupItem(props) {
-    const clock = "clock"
-    const icon = (props.iconType === clock) ? <BsFillClockFill className={styles.__icon} /> : <AiTwotoneHeart className={styles.__icon} />
+    function defineIconType() {
+        return (props.iconType === "clock") ? <BsFillClockFill className={styles.__icon} /> : <AiTwotoneHeart className={styles.__icon} />
+    }
+    const icon = defineIconType()
     return (
         <li className={styles.__item}>
             <a href={props.url} className={`${styles.__link} capitalize`}>{icon} {props.name}</a>
