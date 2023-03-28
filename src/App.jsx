3092d8cb0d4f0283b63748578357122aa8d1d86c
rapha-desenvolvedor <contatosraphaelmoreira@gmail.com>
2023-03-28 11:05:12 -0300

    Import and use UiHomePage inside App Component

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 import StylePage from './pages/styleguide/StylePage'
+import UiHomePage from './pages/home/UiHomePage'
 
 function App() {
 
   return (
     <div className="App">
-      <StylePage />
+      {/* <StylePage /> */}
+      <UiHomePage />
     </div>
   )
 }
