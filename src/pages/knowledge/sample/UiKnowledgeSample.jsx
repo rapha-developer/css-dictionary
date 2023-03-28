f385ac9948499709999cf092eb02a6538e476f34
rapha-desenvolvedor <contatosraphaelmoreira@gmail.com>
2023-03-28 10:59:33 -0300

    Create a new UiKnowledgeSample Component. And styles needed.

---
+++ src/pages/knowledge/sample/UiKnowledgeSample.jsx
@@ -0,0 +1,9 @@
+
+import styles from './style.module.css'
+import UiHeadBar from './../../../components/headbar/UiHeadBar';
+function UiKnowledgeSample() {
+    return (
+        <UiHeadBar />
+    )
+}
+export default UiKnowledgeSample
