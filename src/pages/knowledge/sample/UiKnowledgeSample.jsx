b7fe7017bd18ff9ead9e2fc97414a7e0c8a9ac95
rapha-desenvolvedor <contatosraphaelmoreira@gmail.com>
2023-03-30 19:03:49 -0300

    Install new development environment: vite@4.2.0

--- src/pages/knowledge/sample/UiKnowledgeSample.jsx
+++
@@ -1,9 +0,0 @@
-
-import styles from './style.module.css'
-import UiHeadBar from './../../../components/headbar/UiHeadBar';
-function UiKnowledgeSample() {
-    return (
-        <UiHeadBar />
-    )
-}
-export default UiKnowledgeSample
