c85c68bd9dadf0f380bd93d7bed61857746b84cc
rapha-desenvolvedor <contatosraphaelmoreira@gmail.com>
2023-04-05 19:41:15 -0300

    Import and use UiCategoriesDetailsPage inside the component App. Also import and use the constant: categoryDetailsExample to fill UiCategoriesDetailsPage

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import viteLogo from '/vite.svg'
 import UiSidebar from './components/sidebar/UiSidebar';
 import UiHeadBar from './components/headbar/UiHeadBar';
 import './App.css'
+import UiCategoriesDetailsPage from './pages/categories/details/UiCategoriesDetailsPage';
+import { categoryDetailsExample } from './data/categoryDetails'
 
 function App() {
   const [isShowSidebar, setIsShowSidebar] = useState(false)
@@ -20,6 +22,11 @@ function App() {
         <UiHeadBar 
           changeIsShowSidebar={() => changeIsShowSidebar()}
           isShowSidebar={isShowSidebar}
+        />
+        <UiCategoriesDetailsPage 
+          title={categoryDetailsExample.title}
+          description={categoryDetailsExample.description}
+          sample={categoryDetailsExample.sample}
         />
       </section>
     </div>
