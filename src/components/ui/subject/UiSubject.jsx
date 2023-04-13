d37c125dcf99d8790d1354106ee80f682108560a
rapha-desenvolvedor <contatosraphaelmoreira@gmail.com>
2023-04-13 09:33:06 -0300

    Create a new UiSubject Component. And styles needed.

---
+++ src/components/ui/subject/UiSubject.jsx
@@ -0,0 +1,32 @@
+
+import { decreaseKeyToLessThanFour } from '../../../data/functions'
+import styles from './style.module.css'
+import UiSubjectItem from './item/UiSubjectItem';
+function UiSubject({title, collection}) {
+    const propertiesWithColors = collection.map((property, key) => {
+        const colors = ["blue","green","orange","red"]
+        return {
+            ...property,
+            color: colors[decreaseKeyToLessThanFour(key)],
+        }
+    });
+    const items = propertiesWithColors.map((property) => {
+        return (
+            <UiSubjectItem 
+                key={property.name}
+                url={property.url}
+                color={property.color}
+                name={property.name}
+            />
+        )
+    });
+    return (
+        <div className={styles.subject}>
+            <h3 className={styles.subject__title}>{title}</h3>
+            <ul className={styles.subject__propertyList}>
+                {items}
+            </ul>
+        </div>
+    )
+}
+export default UiSubject
