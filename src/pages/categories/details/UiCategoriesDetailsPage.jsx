634d5becf73a856b93399d5d58b5f73a17c3fcaa
rapha-desenvolvedor <contatosraphaelmoreira@gmail.com>
2023-04-18 10:31:13 -0300

    Import and use two functions: useParams and useLoaderData. Responsible for defining the parameter for which object will be displayed

--- src/pages/categories/details/UiCategoriesDetailsPage.jsx
+++ src/pages/categories/details/UiCategoriesDetailsPage.jsx
@@ -1,19 +1,23 @@
 
+import { useParams, useLoaderData } from 'react-router-dom'
 import styles from './style.module.css'
-function UiCategoriesDetailsPage({title, description, sample}) {
-
+function UiCategoriesDetailsPage() {
+    const snippetCollection = useLoaderData();
+    const { propertyID } = useParams()
+    const snippet = snippetCollection.samples.find(snippetItem => snippetItem.id === propertyID)
     return (
         <section className={styles.uiCategoriesDetails}>
             <div className={styles.uiCategoriesDetails__head}>
-                <h2 className={`${styles.uiCategories__title} capitalize`}>{title}</h2>
-                <p className={styles.uiCategories__description}>{description}</p>
+                <h2 className={`${styles.uiCategories__title} capitalize`}>{snippet.title}</h2>
+                <p className={styles.uiCategories__description}>{snippet.description}</p>
             </div>
             <div className={styles.uiCategoriesDetails__body}>
-                <h3 className={styles.uiCategoriesExample__title}>{sample.title}</h3>
-                <p className={styles.uiCategoriesExample__description}>{sample.description}</p>
+                <h3 className={styles.uiCategoriesExample__title}>{snippet.sample.title}</h3>
+                <p className={styles.uiCategoriesExample__description}>{snippet.sample.description}</p>
                 <div className={styles.uiCategoriesExample__sample}></div>
             </div>
         </section>
     )
 }
+
 export default UiCategoriesDetailsPage
