afdce954738411c5e1a297497a4980440d61c035
rapha-desenvolvedor <contatosraphaelmoreira@gmail.com>
2023-04-18 11:08:24 -0300

    Import two constants: createCSSPropertyCollection and createCSSFunctionsCollection. Refactor the whole createStartWithEncyclopediaPage constant

--- src/data/encyclopedia/createEncyclopediaPage.js
+++ src/data/encyclopedia/createEncyclopediaPage.js
@@ -1,33 +1,10 @@
-import { createPropertiesThatStartWithTheLetterA } from './startWithA.js'
-import { createPropertiesThatStartWithTheLetterB } from './startWithB.js'
-import { createPropertiesThatStartWithTheLetterC } from './startWithC.js'
-import { createPropertiesThatStartWithTheLetterD } from './startWithD.js'
-import { createPropertiesThatStartWithTheLetterE } from './startWithE.js'
-const createStartWithEncyclopediaPage = {
-    title: "CSS Properties",
-    items: [
-        {
-            title: "Começando com A",
-            collection: createPropertiesThatStartWithTheLetterA
-        },
-        {
-            title: "Começando com B",
-            collection: createPropertiesThatStartWithTheLetterB
-        },
-        {
-            title: "Começando com C",
-            collection: createPropertiesThatStartWithTheLetterC
-        },
-        {
-            title: "Começando com D",
-            collection: createPropertiesThatStartWithTheLetterD
-        },
-        {
-            title: "Começando com E",
-            collection: createPropertiesThatStartWithTheLetterE
-        },
-    ]
-}
+import { createCSSPropertyCollection } from './createPropertiesCollection'
+import { createCSSFunctionsCollection } from './createFunctionsCollection'
+
+const createStartWithEncyclopediaPage = [
+    createCSSPropertyCollection,    
+    createCSSFunctionsCollection
+]
 export {
     createStartWithEncyclopediaPage
 }
