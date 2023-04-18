c1131734d3bc0d97a84ba6ae8a19b33e823d4d84
rapha-desenvolvedor <contatosraphaelmoreira@gmail.com>
2023-04-18 11:43:21 -0300

    Use all the features of react-router-dom to build the entire application routing.

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,39 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {
+  createBrowserRouter,
+  RouterProvider
+} from 'react-router-dom'
+import UiEncyclopediaPage, {loader as encyclopediaLoader} from './pages/encyclopedia/UiEncyclopediaPage'
+import UiCategoriesDetailsPage from './pages/categories/details/UiCategoriesDetailsPage'
 import App from './App'
+import UiCategoriesSamples, {loader as sampleLoader} from './pages/categories/samples/UiCategoriesSamples'
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: "ref/:categorySlug",
+        element: <UiEncyclopediaPage />,
+        loader: encyclopediaLoader
+      }, 
+      {
+        path: "ref/:category/:propertyName",
+        element: <UiCategoriesSamples />,
+        loader: sampleLoader,
+      },
+      {
+        path: "ref/:category/:propertyName/:propertyID",
+        element: <UiCategoriesDetailsPage />,
+        loader: sampleLoader
+      }
+    ]
+  }
+])
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
