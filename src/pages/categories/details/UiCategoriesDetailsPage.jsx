b7356de5ad64d56ddd622a78a083db89fd149752
rapha-desenvolvedor <contatosraphaelmoreira@gmail.com>
2023-04-20 18:44:41 -0300

    Import and use React.lazy function to show components library

--- src/pages/categories/details/UiCategoriesDetailsPage.jsx
+++ src/pages/categories/details/UiCategoriesDetailsPage.jsx
@@ -1,10 +1,19 @@
 
+import UiLoading from '../../../components/ui/loading/UiLoading'; 
 import { useParams, useLoaderData } from 'react-router-dom'
 import styles from './style.module.css'
+import { lazy, Suspense, useState } from 'react'
+
 function UiCategoriesDetailsPage() {
     const snippetCollection = useLoaderData();
-    const { propertyID } = useParams()
+    const { category, propertyName, propertyID } = useParams()
     const snippet = snippetCollection.samples.find(snippetItem => snippetItem.id === propertyID)
+    
+    function importComponentByLazy(category, propertyName, propertyID) {
+        const ImportedComponent = lazy(() => import(`../../../library/${category}/a/${propertyName}/sample${propertyID}/Sample.jsx`))
+        return ImportedComponent
+    }
+    const ComponentSample = importComponentByLazy(category, propertyName, propertyID)
     return (
         <section className={styles.uiCategoriesDetails}>
             <div className={styles.uiCategoriesDetails__head}>
@@ -14,7 +23,11 @@ function UiCategoriesDetailsPage() {
             <div className={styles.uiCategoriesDetails__body}>
                 <h3 className={styles.uiCategoriesExample__title}>{snippet.sample.title}</h3>
                 <p className={styles.uiCategoriesExample__description}>{snippet.sample.description}</p>
-                <div className={styles.uiCategoriesExample__sample}></div>
+                <div className={styles.uiCategoriesExample__sample}>
+                    <Suspense fallback={<UiLoading />}>
+                        <ComponentSample />
+                    </Suspense>
+                </div>
             </div>
         </section>
     )
