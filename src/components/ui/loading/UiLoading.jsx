fd397dd4245746bfc74e9c58be8c2729ecdd0c3e
rapha-desenvolvedor <contatosraphaelmoreira@gmail.com>
2023-04-20 18:07:56 -0300

    Create a new UiLoading Component. And styles needed.

---
+++ src/components/ui/loading/UiLoading.jsx
@@ -0,0 +1,10 @@
+
+import styles from './style.module.css'
+function UiLoading() {
+    return (
+        <div className={styles.uiLoading__container}>
+            <div className={styles.loading}></div>
+        </div>
+    )
+}
+export default UiLoading
