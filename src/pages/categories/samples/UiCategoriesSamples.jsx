1d1ab68d67d2ce6359b42b74de6437c69ca4d50c
rapha-desenvolvedor <contatosraphaelmoreira@gmail.com>
2023-04-23 14:40:18 -0300

    Import and use React.lazy function to show thumbnails sample from components library

--- src/pages/categories/samples/UiCategoriesSamples.jsx
+++ src/pages/categories/samples/UiCategoriesSamples.jsx
@@ -1,15 +1,29 @@
-import { useLoaderData, useLocation } from 'react-router-dom'
+import { useLoaderData, useLocation, useParams } from 'react-router-dom'
 import { createSampleOfProperties } from '../../../data/samples/createSampleOfProperties'
+import { lazy, Suspense} from 'react'
+import UiLoading from '../../../components/ui/loading/UiLoading'
 import styles from './style.module.css'
 function UiCategoriesSamples() {
     const category = useLoaderData()
-    const location = useLocation() 
+    const location = useLocation()
+    const params = useParams() 
+
+    function importComponentByLazy(category, propertyName, propertyID) {
+        const ImportedComponent = lazy(() => import(`../../../library/${category}/a/${propertyName}/sample${propertyID}/thumbnail/SampleThumbnail.jsx`))
+        return ImportedComponent
+    }
+
     const snippet__list = category.samples.map((snippetItem) => {
+        const ComponentSample = importComponentByLazy(params.category, params.propertyName, snippetItem.id)
         const fullPath = (`${location.pathname}/${snippetItem.id}`)
         return (
             <a  href={fullPath}
                 className={styles.snippetItem} key={snippetItem.id}>
-                <div className={styles.snippetItem__thumbnail}></div>
+                <div className={styles.snippetItem__thumbnail}>
+                    <Suspense fallback={<UiLoading />}>
+                        <ComponentSample />
+                    </Suspense>
+                </div>
                 <span className={`${styles.snippetItem__date} uppercase`}>{snippetItem.date}</span>
                 <h3 className={styles.snippetItem__title}>{snippetItem.title}</h3>
                 <p className={styles.snippetItem__description}>{snippetItem.description}</p>
