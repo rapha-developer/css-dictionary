9b820e873ec3d68f037dc065986866171b728887
rapha-desenvolvedor <contatosraphaelmoreira@gmail.com>
2023-04-24 12:29:37 -0300

    Save the Sample2 [group of elements] of Accent-color property in library

---
+++ src/library/css-properties/a/accent-color/sample2/Sample.jsx
@@ -0,0 +1,17 @@
+
+import UiElement from '../../../../../components/ui/element/UiElement'
+import UiTemplateSetInnerHTML from '../../../../../components/ui/templateSetInner/UiTemplateSetInnerHTML'
+import htmlRaw from './sample.html?raw'
+import styles from './style.css?inline'
+import './style.css'
+function Sample() {
+    const sampleComponent = <UiTemplateSetInnerHTML html={htmlRaw} />
+    return (
+        <UiElement 
+            codeCSS={styles}
+            codeHTML={htmlRaw}
+            component={sampleComponent}
+        />
+    )
+}
+export default Sample
