13828de8c9ba0ed2d3e805bec4488ad985678af5
rapha-desenvolvedor <contatosraphaelmoreira@gmail.com>
2023-04-25 18:42:17 -0300

    Feature: Build autocomplete with all properties store in data/encyclopedia folder

--- src/components/headbar/form/UiHeadBarForm.jsx
+++ src/components/headbar/form/UiHeadBarForm.jsx
@@ -1,14 +1,55 @@
 
+import { useEffect, useState } from 'react'
+import { generateInputData, allPropertiesFromEncyclopediaPage } from '../../../data/autocompleteBar.js'
 import styles from './style.module.css'
-function UiHeadBarForm(props) {
+import UiHeadBarAutocomplete from '../autocomplete/UiHeadBarAutocomplete'
+function UiHeadBarForm() {
+    const [saveResponseAPI, setSaveResponseAPI] = useState([])
+    useEffect(() => {
+        setSaveResponseAPI(allPropertiesFromEncyclopediaPage())
+    }, [])
+
+    const [formData, setFormData] = useState(generateInputData())     
+    function onChangeInput(event) {
+        const { id, name, type, value } = event.target
+        setFormData((prevFromData) => {
+            return {
+                ...prevFromData,
+                value: value.toLowerCase()
+            }
+        });
+    }
+    function filterPropertiesBySearchInput() {
+        return saveResponseAPI.filter((item) => item.slug.substring(0, formData.value.length).toLowerCase() === formData.value)
+    }
+    const [itemsFounded, setItemsFounded] = useState([])
+    useEffect(() => {
+        if (formData.value.length === 0) return;
+        const itemsAfterFilter = filterPropertiesBySearchInput()
+        setItemsFounded(itemsAfterFilter)
+    }, [formData.value])
+
+    const autocompleteIsVisible = (formData.value.length > 0) ? true : false
+
     return (
-        <input
-            id={"ui-head-bar-input-id"}
-            name={"ui-head-bar-input-name"}
-            placeholder={"search"}
-            type={"text"}
-            className={styles.UiHeadBarForm__input}
-        />
+        <form action="#" 
+            autoComplete='off'
+            className={styles.UiHeadBarForm}>
+            <div className={styles.UiHeadBarForm__group}>
+                <input 
+                    id={formData.id}
+                    name={formData.name}
+                    placeholder={formData.placeholder}
+                    type={"text"}
+                    value={formData.value}
+                    onChange={(event) => onChangeInput(event)}
+                    className={styles.UiHeadBarForm__input}
+                />
+            {autocompleteIsVisible && <UiHeadBarAutocomplete 
+                items={itemsFounded}
+            />}
+            </div>
+        </form>
     )
 }
 export default UiHeadBarForm
