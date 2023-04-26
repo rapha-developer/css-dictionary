8d0c95f68688e3265066df1361373fdf04a1ec7b
rapha-desenvolvedor <contatosraphaelmoreira@gmail.com>
2023-04-26 11:12:35 -0300

    Create option 'no property is found' for default case: when there are no options

--- src/components/headbar/autocomplete/UiHeadBarAutocomplete.jsx
+++ src/components/headbar/autocomplete/UiHeadBarAutocomplete.jsx
@@ -1,6 +1,7 @@
 
 import styles from './style.module.css'
 function UiHeadBarAutocomplete({items}) {
+    const noPropertyFound = (items.length === 0) ? true : false
     const autocomplete__items = items.map((item, key) => {
         const url = `/ref/${item.category}/${item.slug}`
         return (
@@ -9,9 +10,18 @@ function UiHeadBarAutocomplete({items}) {
             </li>
         )
     });
+    function generateNoPropertyResponse() {
+        const noPropertyText = "No property is found"
+        return (
+            <li className={styles.uiHeadBarAutocomplete__item} data-disable="true">
+                <a href="" className={styles.uiHeadBarAutocomplete__link}>{noPropertyText}</a>
+            </li>
+        )
+    }
     return ( 
         <ul className={styles.uiHeadBarAutocomplete}>
             {autocomplete__items}
+            {noPropertyFound && generateNoPropertyResponse()}
         </ul>
     )
 }
