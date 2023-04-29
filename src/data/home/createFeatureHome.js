bcdbca4c770a7a24d08ce4489fa8b517176697c3
rapha-desenvolvedor <contatosraphaelmoreira@gmail.com>
2023-04-29 18:38:58 -0300

    Create and export three new files: createFeaturesHome, createMainHome and createTeamHome. Each file have one or more constants used in UiHome Component

---
+++ src/data/home/createFeatureHome.js
@@ -0,0 +1,25 @@
+const createUiHomeFeatures = {
+    headline: "Por que usar esse software?",
+    description: "Por vários motivos você pode utilizar essa aplicação, dentre eles estão:",
+    items: [
+        {
+            icon: "started",
+            title: "Getting Started",
+            description: "Comece rápido com pesquisas usando autocomplete, divididos por categorias"
+        },
+        {
+            icon: "library",
+            title: "CSS Library",
+            description: "Aprenda tudo sobre as propriedades CSS, funções e como usá-las"
+        },
+        {
+            icon: "copy-and-past",
+            title: "Copy and Past",
+            description: "Copie o código fonte HTML ou CSS para seu projeto como desejar"
+        }
+    ]
+}
+
+export {
+    createUiHomeFeatures
+}
