6a74328dbacf4765dacd0867a9fff1b3ff78bc53
rapha-desenvolvedor <contatosraphaelmoreira@gmail.com>
2023-04-30 13:37:14 -0300

    Create and export three new constant(s): createUiFooterProjects, createUiFooterSocialMedia and createUiFooterCopyright; Used to fill UiFooter children components

---
+++ src/data/footer.js
@@ -0,0 +1,35 @@
+const createUiFooterProjects = [
+    {
+        name: "Tenzies Game",
+        url: "https://github.com/rapha-developer/tenzies-game"
+    },
+    {
+        name: "Behance Portfolio",
+        url: "https://github.com/rapha-developer/behance-portfolio"
+    },
+    {
+        name: "Api's to consume",
+        url: "https://github.com/rapha-developer/APIs-to-consume"
+    },
+]
+const createUiFooterSocialMedia = {
+    codepen: "https://www.codepen.io/rapha-developer",
+    behance: "https://www.behance.net/rapha-developer",
+    github: "https://github.com/rapha-developer"
+}
+const createUiFooterCopyright = {
+    text: "Made by @rapha-developer Studio in Brazil.",
+    jekyll: {
+        name: "jekyll docs",
+        url: "https://jekyll-theme-docs.netlify.app/"
+    },
+    boxboard: {
+        name: "boxboard dashboard",
+        url: "https://www.figma.com/community/file/1000409630349340331/Boxboard-Dashboard-Ui-Kit-Preview"
+    }
+}
+export {
+    createUiFooterProjects,
+    createUiFooterSocialMedia,
+    createUiFooterCopyright
+}
