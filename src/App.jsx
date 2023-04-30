92828a3b8e4396ed8e806bd648f7560d965c0d79
rapha-desenvolvedor <contatosraphaelmoreira@gmail.com>
2023-04-30 14:36:40 -0300

    Create loader function and useLoaderData function to generate dynamicallyeach headline in UiHeadBar

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,12 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import UiSidebar from './components/sidebar/UiSidebar';
 import UiHeadBar from './components/headbar/UiHeadBar';
-import { Outlet } from 'react-router-dom'
+import { Outlet, useLoaderData } from 'react-router-dom'
 import './App.css'
 import UiFooter from './components/footer/UiFooter';
 
 function App() {
+  const { headline } = useLoaderData() 
   const [isShowSidebar, setIsShowSidebar] = useState(false)
   function changeIsShowSidebar() {
     setIsShowSidebar(prevState => !prevState)
@@ -22,6 +23,7 @@ function App() {
         <UiHeadBar 
           changeIsShowSidebar={() => changeIsShowSidebar()}
           isShowSidebar={isShowSidebar}
+          headline={headline}
         />
         <Outlet />
         <UiFooter />
@@ -29,5 +31,28 @@ function App() {
     </div>
   )
 }
+export async function loader({ params }) {
+  const allSlugNames = Object.keys(params)
+  function generateFormattedResponse(headline__text) {
+    return {
+      headline: headline__text
+    }
+  }
 
+  const paramsAreEmpty = (allSlugNames.length === 0) ? true : false
+  if(paramsAreEmpty) {
+    return generateFormattedResponse("Home")
+  }
+  const lastSlugName = allSlugNames[allSlugNames.length - 1]
+  if (lastSlugName === "categorySlug") {
+    return generateFormattedResponse("Category list")
+  }
+  if (lastSlugName === "propertyName") {
+    return generateFormattedResponse("Sample list")
+  }
+  if (lastSlugName === "propertyID") {
+    return generateFormattedResponse("Property details")
+  }
+  return generateFormattedResponse("Css dictionary")
+}
 export default App
