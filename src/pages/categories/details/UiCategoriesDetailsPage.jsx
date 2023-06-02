977b4e5fcbc1e95c9d048d2fc897ad4ec74d83e9
rapha-desenvolvedor <contatosraphaelmoreira@gmail.com>
2023-06-02 18:30:19 -0300

    Create and export loader function with data collected from an external api. Use useLoaderData to fill component with the appropriate collected information

--- src/pages/categories/details/UiCategoriesDetailsPage.jsx
+++ src/pages/categories/details/UiCategoriesDetailsPage.jsx
@@ -5,9 +5,8 @@ import styles from './style.module.css'
 import { lazy, Suspense, useState } from 'react'
 
 function UiCategoriesDetailsPage() {
-    const snippetCollection = useLoaderData();
+    const responseSingleSample = useLoaderData();
     const { category, propertyName, propertyID } = useParams()
-    const snippet = snippetCollection.samples.find(snippetItem => snippetItem.id === propertyID)
     
     function importComponentByLazy(category, propertyName, propertyID) {
         const ImportedComponent = lazy(() => import(`../../../library/${category}/a/${propertyName}/sample${propertyID}/Sample.jsx`))
@@ -17,12 +16,20 @@ function UiCategoriesDetailsPage() {
     return (
         <section className={styles.uiCategoriesDetails}>
             <div className={styles.uiCategoriesDetails__head}>
-                <h2 className={`${styles.uiCategories__title} capitalize`}>{snippet.title}</h2>
-                <p className={styles.uiCategories__description}>{snippet.description}</p>
+                <h2 className={`${styles.uiCategories__title} capitalize`}>
+                    {responseSingleSample.data.attributes.title}
+                </h2>
+                <p className={styles.uiCategories__description}>
+                    {responseSingleSample.data.attributes.description}
+                </p>
             </div>
             <div className={styles.uiCategoriesDetails__body}>
-                <h3 className={styles.uiCategoriesExample__title}>{snippet.sample.title}</h3>
-                <p className={styles.uiCategoriesExample__description}>{snippet.sample.description}</p>
+                <h3 className={styles.uiCategoriesExample__title}>
+                    {responseSingleSample.data.display.title}
+                </h3>
+                <p className={styles.uiCategoriesExample__description}>
+                    {responseSingleSample.data.display.description_pt}
+                </p>
                 <div className={styles.uiCategoriesExample__sample}>
                     <Suspense fallback={<UiLoading />}>
                         <ComponentSample />
@@ -31,6 +38,12 @@ function UiCategoriesDetailsPage() {
             </div>
         </section>
     )
+}
+export async function loader({ params }) {
+    const url = `https://rapha-developer-laravel.000webhostapp.com/properties/${params.propertyName}/singleSample/${params.propertyID}`;
+    const responseFromApi = await fetch(url);
+	const responseInJson = await responseFromApi.json();
+    return responseInJson
 }
 
 export default UiCategoriesDetailsPage
