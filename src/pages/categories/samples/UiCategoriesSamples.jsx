6f0d8200da2c209fc56c123e98946b981a436312
rapha-desenvolvedor <contatosraphaelmoreira@gmail.com>
2023-06-02 18:32:37 -0300

    Make loader function export data collected from an external api. Use useLoaderData to fill component with the appropriate collected information

--- src/pages/categories/samples/UiCategoriesSamples.jsx
+++ src/pages/categories/samples/UiCategoriesSamples.jsx
@@ -1,49 +1,72 @@
-import { useLoaderData, useLocation, useParams } from 'react-router-dom'
-import { createSampleOfProperties } from '../../../data/samples/createSampleOfProperties'
-import { lazy, Suspense} from 'react'
-import UiLoading from '../../../components/ui/loading/UiLoading'
-import styles from './style.module.css'
+import { useLoaderData, useLocation, useParams } from "react-router-dom";
+import { lazy, Suspense, useState } from "react";
+import UiLoading from "../../../components/ui/loading/UiLoading";
+import styles from "./style.module.css";
 function UiCategoriesSamples() {
-    const category = useLoaderData()
-    const location = useLocation()
-    const params = useParams() 
+	const responseAllSamples = useLoaderData();
+	const name = responseAllSamples.data[0].relationships.name
+	const location = useLocation();
+	const params = useParams();
+	function importComponentByLazy(category, propertyName, propertyID) {
+		const ImportedComponent = lazy(() =>
+			import(
+				`../../../library/${category}/a/${propertyName}/sample${propertyID}/thumbnail/SampleThumbnail.jsx`
+			)
+		);
+		return ImportedComponent;
+	}
 
-    function importComponentByLazy(category, propertyName, propertyID) {
-        const ImportedComponent = lazy(() => import(`../../../library/${category}/a/${propertyName}/sample${propertyID}/thumbnail/SampleThumbnail.jsx`))
-        return ImportedComponent
-    }
-
-    const snippet__list = category.samples.map((snippetItem) => {
-        const ComponentSample = importComponentByLazy(params.category, params.propertyName, snippetItem.id)
-        const fullPath = (`${location.pathname}/${snippetItem.id}`)
-        return (
-            <a  href={fullPath}
-                className={styles.snippetItem} key={snippetItem.id}>
-                <div className={styles.snippetItem__thumbnail}>
-                    <Suspense fallback={<UiLoading />}>
-                        <ComponentSample />
-                    </Suspense>
-                </div>
-                <span className={`${styles.snippetItem__date} uppercase`}>{snippetItem.date}</span>
-                <h3 className={styles.snippetItem__title}>{snippetItem.title}</h3>
-                <p className={styles.snippetItem__description}>{snippetItem.description}</p>
-            </a>
-        )
-    });
-    const headText = `${category.propertyName} documentation`
-    return (
-        <section className={styles.uiCategoriesSamples}>
-            <div className={styles.uiCategoriesSamples__head}>
-                <h2 className={`${styles.uiCategoriesSamples__title} capitalize`}>{headText}</h2>
-            </div>
-            <div className={styles.__body}>
-                {snippet__list}
-            </div>
-        </section>
-    )
+	const snippet__list = responseAllSamples.data.map((snippetItem) => {
+		const ComponentSample = importComponentByLazy(
+			params.category,
+			params.propertyName,
+			snippetItem.id
+		);
+		const fullPath = `${location.pathname}/${snippetItem.id}`;
+		const dateTime = `${snippetItem.attributes.created_at.date}, ${snippetItem.attributes.created_at.time}`
+		return (
+			<a
+				href={fullPath}
+				className={styles.snippetItem}
+				key={snippetItem.id}
+			>
+				<div className={styles.snippetItem__thumbnail}>
+					<Suspense fallback={<UiLoading />}>
+						<ComponentSample />
+					</Suspense>
+				</div>
+				<span className={`${styles.snippetItem__date}`}>
+					{dateTime}
+				</span>
+				<h3 className={styles.snippetItem__title}>
+					{snippetItem.attributes.title}
+				</h3>
+				<p className={styles.snippetItem__description}>
+					{snippetItem.attributes.description}
+				</p>
+			</a>
+		);
+	});
+	const headText = `${name} documentation`;
+	return (
+		<section className={styles.uiCategoriesSamples}>
+			<div className={styles.uiCategoriesSamples__head}>
+				<h2
+					className={`${styles.uiCategoriesSamples__title} capitalize`}
+				>
+					{headText}
+				</h2>
+			</div>
+			<div className={styles.__body}>{snippet__list}</div>
+		</section>
+	);
 }
 export async function loader({ params }) {
-    const snippets = createSampleOfProperties.find((sampleItem) => sampleItem.propertyName === params.propertyName)
-    return snippets
+
+	const url = `https://rapha-developer-laravel.000webhostapp.com/properties/${params.propertyName}/allSamples`;
+	const responseFromApi = await fetch(url);
+	const responseInJson = await responseFromApi.json();	
+	return responseInJson;
 }
-export default UiCategoriesSamples
+
+export default UiCategoriesSamples;
