253f9f66accd66879e886d0a7f02328d4a042f06
rapha-desenvolvedor <contatosraphaelmoreira@gmail.com>
2023-06-06 11:32:16 -0300

    [AutocompleteBar Step: 1] Refactor placeholder attribute of the generateInputData. And remove unnecessary import, function and export

--- src/data/autocompleteBar.js
+++ src/data/autocompleteBar.js
@@ -1,29 +1,11 @@
-import { createStartWithEncyclopediaPage } from './encyclopedia/createEncyclopediaPage'
 function generateInputData() {
     return {
         id: "ui-head-bar-input-id",
         name: "ui-head-bar-input-name",
-        placeholder: 'search properties using [-] between words',
+        placeholder: 'search properties by name',
         value: "",
     }
 }
-function allPropertiesFromEncyclopediaPage() {
-    const allProperty = []
-    createStartWithEncyclopediaPage.forEach((encyclopediaItem) => {
-        const slugFromCategory = encyclopediaItem.slug
-        encyclopediaItem.items.forEach((item) => {
-            item.collection.forEach((property) => {
-                allProperty.push({
-                    name: property.name,
-                    slug: property.slug,
-                    category:slugFromCategory
-                });
-            })
-        })
-    });
-    return allProperty
-}
 export {
-    generateInputData,
-    allPropertiesFromEncyclopediaPage
+    generateInputData
 }
