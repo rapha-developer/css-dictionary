30f43b9bee3def73c2f82c84350997ce7a252b72
rapha-desenvolvedor <contatosraphaelmoreira@gmail.com>
2023-08-25 16:10:54 -0300

    [Data step:1] create and export two arrays (fake data) to build Feed and Sidebar Items.

---
+++ src/data/ItemsData.tsx
@@ -0,0 +1,49 @@
+import { feedItem } from "../types/feedItem";
+import { sidebarItem } from "../types/sidebarItem";
+
+export const feedItems:Array<feedItem> = [
+    {
+        url: "#",
+        text: "art"
+    },
+    {
+        url: "#",
+        text: "music"
+    },
+    {
+        url: "#",
+        text: "Collectibles"
+    },
+    {
+        url: "#",
+        text: "see more"
+    },
+];
+
+export const sidebarItems:Array<sidebarItem> = [
+    {
+        url: "#",
+        text: "home",
+        iconName: "home",
+    },
+    {
+        url: "#",
+        text: "properties",
+        iconName: "square3d"
+    },
+    {
+        url: "#",
+        text: "functions",
+        iconName: "square3d"
+    },
+    {
+        url: "#",
+        text: "glossary",
+        iconName: "academicCap"
+    },
+    {
+        url: "#",
+        text: "FAQ",
+        iconName: "bookOpen"
+    },
+];
