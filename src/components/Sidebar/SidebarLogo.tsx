70d0d003584ae8add2335ff606a6b11e5270c226
rapha-desenvolvedor <contatosraphaelmoreira@gmail.com>
2023-08-25 16:20:15 -0300

    [Sidebar step:1] Create Sidebar Logo component

---
+++ src/components/Sidebar/SidebarLogo.tsx
@@ -0,0 +1,10 @@
+
+
+export const SidebarLogo = () => {
+    return (
+        <img src="/css-logo.svg" 
+            alt="Css Dictionary Logo" 
+            className="w-14 sm:w-24 mx-auto"
+        />
+    )
+}
