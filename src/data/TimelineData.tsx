b018e8984344c8924e2ce225bc483c2613277af1
rapha-desenvolvedor <contatosraphaelmoreira@gmail.com>
2023-08-26 16:12:26 -0300

    [data step:1] create data (post collection) of timeline component

---
+++ src/data/TimelineData.tsx
@@ -0,0 +1,15 @@
+
+import { feedTimelineItem } from "../types/feedTimelineItem";
+
+export const timelineData:Array<feedTimelineItem> = [
+    {
+        title: "Abstract Girl",
+        description: "Check examples",
+        image: "/thumbnail-property.svg"
+    },
+    {
+        title: "Beautiful Flower",
+        description: "Check examples",
+        image: "/thumbnail-property.svg"
+    }
+]
