5a68c0b0f529614ee8a6d9999b6a88b1d902115f
rapha-desenvolvedor <contatosraphaelmoreira@gmail.com>
2023-08-26 16:38:22 -0300

    [App.tsx step:1] App design: template with sidebar and content (side by side)

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import './App.css'
+import { Feed } from './components/Feed'
+import { Header } from './components/Header'
+import { Sidebar } from './components/Sidebar'
 
-function App() {
+const App = () => {
 
   return (
-    <>
-      <h1>Hello world</h1>
-    </>
+    <div className='App bg-backgrounds-500 min-h-screen'>
+      <Sidebar />
+      <div className="container mx-auto">
+        <Header />
+        <Feed />
+      </div>
+    </div> 
   )
 }
 
