46c07494ba20c76cdce2de763da6dac2b4ce142d
rapha-desenvolvedor <contatosraphaelmoreira@gmail.com>
2023-08-27 13:29:09 -0300

    [Data step:1] create new two posts (feedTimelineItem)

--- src/data/TimelineData.tsx
+++ src/data/TimelineData.tsx
@@ -11,5 +11,15 @@ export const timelineData:Array<feedTimelineItem> = [
         title: "Beautiful Flower",
         description: "Check examples",
         image: "/thumbnail-property.svg"
-    }
+    },
+    {
+        title: "Abstract person",
+        description: "Check examples",
+        image: "/thumbnail-property.svg"
+    },
+    {
+        title: "Beautiful Forest",
+        description: "Check examples",
+        image: "/thumbnail-property.svg"
+    },
 ]
