b31d38e0dc9ac19abd0ced4f90779ea12f703842
rapha-desenvolvedor <contatosraphaelmoreira@gmail.com>
2023-09-04 19:54:42 -0300

    [propertyCard step:1] create PropertyCard component. Used as a template to show each property coming from the api

---
+++ src/components/Card/PropertyCard.tsx
@@ -0,0 +1,29 @@
+import { Property } from "../../types/property";
+
+
+export const PropertyCard = ({ attributes }: Property) => {
+    const image = (attributes.category === "css-functions") ? "ManTouch3dOptions.png" : "WomanWithPhone.png"
+	return (
+		<div
+			className="propertyCard elevation-card-1 flex flex-col rounded-s-2xl">
+			<img
+				src={`/${image}`}
+				alt="Woman with phone"
+				className="w-4/5 h-auto top-20 absolute mx-auto -z-0"
+			/>
+			<header className="propertyCard-header z-0">
+				<p className="propertyCard-date">
+					{attributes.created_at}
+				</p>
+				<h2 className="propertyCard-name capitalize">
+					{attributes.name}
+				</h2>
+			</header>
+			<div className="propertyCard-content">
+				<p className="propertyCard-description">
+					{attributes.description}
+				</p>
+			</div>
+		</div>
+	);
+};
