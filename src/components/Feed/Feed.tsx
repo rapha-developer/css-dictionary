01e249f786a7211eaf1301e3684adc72e218c624
rapha-desenvolvedor <contatosraphaelmoreira@gmail.com>
2023-09-04 19:37:49 -0300

    [Feed step:1] Import queryAtom and change variable in screen when user typing

--- src/components/Feed/Feed.tsx
+++ src/components/Feed/Feed.tsx
@@ -1,9 +1,12 @@
 
+import { useAtom } from "jotai"
 import { FeedFilters } from "./FeedFilters"
 import { FeedTimeline } from "./FeedTimeline"
+import { queryAtom } from "../../atoms"
 
 export const Feed = () => {
-    const title = "trending nFT’s"
+    const [query] = useAtom(queryAtom)
+    const title = (query === "") ? "trending nFT’s" : "You search: " + query
     return (
         <section className="px-4 lg:px-10">
             <div className="flex flex-col sm:flex-row justify-between mb-6 sm:mb-8">
