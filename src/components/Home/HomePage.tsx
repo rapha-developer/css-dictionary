0e393ecba6af4177bb3aec5f16ccf237ce375127
rapha-desenvolvedor <contatosraphaelmoreira@gmail.com>
2023-09-06 10:14:50 -0300

    [HomePage step:1] Import and use HomeSearch component inside the HomePage component

--- src/components/Home/HomePage.tsx
+++ src/components/Home/HomePage.tsx
@@ -1,9 +1,11 @@
 
 import { HomeCollection } from "./HomeCollection"
+import { HomeSearch } from "./HomeSearch"
 
 export const HomePage = () => {
     return (
         <section className="px-4 lg:px-10">
+            <HomeSearch />
             <HomeCollection 
                 key={'kFirstTrending'}
                 url="https://rapha-developer-laravel.000webhostapp.com/all_properties"
