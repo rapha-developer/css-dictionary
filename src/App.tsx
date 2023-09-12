5a547938d927ec060cad1e3dd7eb194574437159
rapha-desenvolvedor <contatosraphaelmoreira@gmail.com>
2023-09-12 19:22:29 -0300

    [App step:1] Import BrowserRoutes, Routes and others functions to create application routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,26 @@
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import './App.css'
 import { Header } from './components/Header'
 import { HomePage } from './components/Home/HomePage'
 import { Sidebar } from './components/Sidebar'
+import { PropertyPage } from './components/Property/PropertyPage'
 
 const App = () => {
 
   return (
+    <BrowserRouter>
     <div className='App bg-backgrounds-500 min-h-screen'>
       <Sidebar />
       <div className="container mx-auto">
         <Header />
-        <HomePage />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/property/:id" element={<PropertyPage />} />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
       </div>
-    </div> 
+    </div>
+    </BrowserRouter>
   )
 }
 
