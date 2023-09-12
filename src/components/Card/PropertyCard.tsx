6718c0eb4127198cabf998ddef13561f6b01c4d0
rapha-desenvolvedor <contatosraphaelmoreira@gmail.com>
2023-09-12 19:20:25 -0300

    [PropertyCard step:1] Import Link and use to navigate between collection property

--- src/components/Card/PropertyCard.tsx
+++ src/components/Card/PropertyCard.tsx
@@ -1,16 +1,19 @@
+import { Link } from "react-router-dom";
 import { Property } from "../../types/property";
 
 
-export const PropertyCard = ({ attributes }: Property) => {
+export const PropertyCard = ({ attributes, id }: Property) => {
     const image = (attributes.category === "css-functions") ? "ManTouch3dOptions.png" : "WomanWithPhone.png"
 	return (
 		<div
 			className="propertyCard elevation-card-1 flex flex-col rounded-s-2xl">
+			<Link to={`/property/${id}`} preventScrollReset={true}>
 			<img
 				src={`/${image}`}
 				alt="Woman with phone"
 				className="w-4/5 h-auto top-20 absolute mx-auto -z-0"
 			/>
+			</Link>
 			<header className="propertyCard-header z-0">
 				<p className="propertyCard-date">
 					{attributes.created_at}
