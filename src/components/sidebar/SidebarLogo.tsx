a9aad4ad34eea3065c28a2206092eb0dd1cd7ef9
rapha-desenvolvedor <contatosraphaelmoreira@gmail.com>
2023-09-22 19:31:53 -0300

    [sidebar] Create Sidebar Logo component, which shows logo inside

---
+++ src/components/sidebar/SidebarLogo.tsx
@@ -0,0 +1,9 @@
+
+export const SidebarLogo = () => {
+    return (
+        <img src="/LogoCSS.svg" 
+            alt="Sidebar Logo" 
+            className="w-14 h-16 mb-10 mx-auto"
+        />
+    )
+}
