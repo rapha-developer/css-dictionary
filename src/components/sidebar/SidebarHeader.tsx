3b60a7302eb357e496ab60f0e97bc32f63998299
rapha-desenvolvedor <contatosraphaelmoreira@gmail.com>
2023-09-22 19:32:41 -0300

    [sidebar] Create Sidebar Header component, which shows logo inside

---
+++ src/components/sidebar/SidebarHeader.tsx
@@ -0,0 +1,10 @@
+import { SidebarLogo } from "./SidebarLogo"
+
+
+export const SidebarHeader = () => {
+    return (
+        <header className="mb-3">
+            <SidebarLogo />
+        </header>
+    )
+}
