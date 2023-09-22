68f1c7ccc1441c7de0c39cdeac5921b46b74cdd8
rapha-desenvolvedor <contatosraphaelmoreira@gmail.com>
2023-09-22 19:35:25 -0300

    [sidebar] Create Sidebar Main Menu component, which shows options from main menu

---
+++ src/components/sidebar/SidebarMainMenu.tsx
@@ -0,0 +1,21 @@
+import { SidebarMenuDataList } from "../../data/SidebarData";
+import { SidebarMenuItem } from "./SidebarMenuItem";
+
+export const SidebarMainMenu = () => {
+    const sidebarItems = SidebarMenuDataList
+
+    const empty = 0
+    return (
+        <ul className="flex flex-col gap-10">
+            {Array.isArray(sidebarItems) &&
+            sidebarItems.length > empty &&
+            sidebarItems.map(sidebarItem => (
+                <SidebarMenuItem 
+                    key={`kSidebarItem-${sidebarItem.typeIcon}`}
+                    typeIcon={sidebarItem.typeIcon}
+                />
+            ))
+            }
+        </ul>
+    )
+}
