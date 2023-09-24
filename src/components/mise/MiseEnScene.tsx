8f8e8ae34773d55830d8531099efe150cd15d379
rapha-desenvolvedor <contatosraphaelmoreira@gmail.com>
2023-09-24 15:07:09 -0300

    [mise] Create MiseEnScene component. Responsible for show image in background in each page

---
+++ src/components/mise/MiseEnScene.tsx
@@ -0,0 +1,8 @@
+
+export const MiseEnScene = () => {
+    return (
+        <img src="/bgMainMountain.jpg" 
+            alt="Mountain at night (Montanha sob a noite)" 
+            className="w-full h-[640px] object-cover -z-10 absolute top-0 left-0" />
+    )
+}
