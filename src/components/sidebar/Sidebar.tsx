256752b80963136b23536a308ef3b0fabea4942c
rapha-desenvolvedor <contatosraphaelmoreira@gmail.com>
2023-09-24 11:39:31 -0300

    [sidebar] Add z-index property of sidebar

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -5,7 +5,7 @@ import { SidebarMainMenu } from './SidebarMainMenu';
 
 export const Sidebar = () => {
     return (
-        <aside className="fixed w-[72px] my-4 md:my-8 mx-4 md:mx-8 sidebar-height py-4 sidebar-attributes flex flex-col">
+        <aside className="fixed w-[72px] my-4 md:my-8 mx-4 md:mx-8 sidebar-height py-4 sidebar-attributes flex flex-col z-10">
             <SidebarHeader />
             <SidebarMainMenu />
             <hr className="sidebar-hr"  />
