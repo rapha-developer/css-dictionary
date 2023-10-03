185d33d704dfd9f98a5c9cd78d4e174141b6977f
rapha-desenvolvedor <contatosraphaelmoreira@gmail.com>
2023-10-03 11:49:55 -0300

    [mise] Create CategoriesMiseEnScene component. Responsible for show image in background categories section

---
+++ src/components/categories/CategoriesMiseEnScene.tsx
@@ -0,0 +1,12 @@
+
+
+export const CategoriesMiseEnScene = () => {
+    return (
+        <div className="w-full h-full relative">
+            <img src="bgCategoriesFull.svg" 
+            alt="mountain over a late afternoon sun (montanha ao longo de um sol no fim de tarde)" 
+            className="w-full h-[310px] md:h-[720px] xl:h-[960px] object-cover -z-10 absolute top-20 md:top-0 left-0" />
+            <div className="w-full h-[960px] -z-[9] absolute top-0 left-0 dark-gradient-categories"></div>
+        </div>
+    )
+}
