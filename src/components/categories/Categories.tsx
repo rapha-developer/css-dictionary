872b3f97910476999020716d8e7c6554e2b35c2a
rapha-desenvolvedor <contatosraphaelmoreira@gmail.com>
2023-10-04 12:21:11 -0300

    [categories] Create another div layer, and define grid layout to all children components

--- src/components/categories/Categories.tsx
+++ src/components/categories/Categories.tsx
@@ -1,4 +1,5 @@
 import { Hero } from "../../types/hero"
+import { CategoriesCollection } from "./CategoriesCollection"
 import { CategoriesHero } from "./CategoriesHero"
 import { CategoriesMiseEnScene } from "./CategoriesMiseEnScene"
 
@@ -7,14 +8,19 @@ type Props = {
 }
 export const Categories = ({ hero }: Props) => {
     return (
-        <div className="h-full ui-categories relative">
+        <div className="ui-categories relative h-full">
             <CategoriesMiseEnScene />
-            <CategoriesHero 
-                key={`k${hero.title}`}
-                title={hero.title}
-                subtitle={hero.subtitle}
-                description={hero.description}
-            />
+            <div className="pt-4 sm:pt-0 sm:mt-28 xl:mt-8 h-fit xl:h-[750px] grid grid-cols-1 gap-2 xl:grid-cols-[60%_auto] xl:gap-none items-center">
+                <CategoriesHero 
+                    key={`k${hero.title}`}
+                    title={hero.title}
+                    subtitle={hero.subtitle}
+                    description={hero.description}
+                />
+                <CategoriesCollection 
+                    url="https://rapha-developer-laravel.000webhostapp.com/properties/alphabet"
+                />
+            </div>
         </div>
     )
 }
