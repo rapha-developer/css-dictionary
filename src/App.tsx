61c3388df2599b669866cce7692b6a1b3a0ad61d
rapha-desenvolvedor <contatosraphaelmoreira@gmail.com>
2023-10-06 22:49:06 -0300

    [app] Make all routes necessary for app work

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 import './App.css'
+import { HomePage } from './components/home/HomePage'
+import { Sidebar } from './components/sidebar'
+import { HomeCourses } from './components/home/HomeCourses'
 
 function App() {
 
   return (
-    <>
-      <h1 className="">Hello World</h1>
-    </>
+    <BrowserRouter>
+    <div className='App'>
+      <Sidebar />
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/collection/:letter' element={<HomeCourses />} />
+        <Route path='*' element={<Navigate to="/" />} />
+      </Routes> 
+    </div>
+    </BrowserRouter>
   )
 }
 
