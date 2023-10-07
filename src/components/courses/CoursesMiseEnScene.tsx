cba91d998bfcfcee8646bcc27e25a3b22fd6b0ba
rapha-desenvolvedor <contatosraphaelmoreira@gmail.com>
2023-10-06 21:58:27 -0300

    [mise] Create courseMiseEnScene component, responsible for show background image of the homeCourse

---
+++ src/components/courses/CoursesMiseEnScene.tsx
@@ -0,0 +1,9 @@
+
+
+export const CoursesMiseEnScene = () => {
+    return (
+        <img src="/collection-banner.jpg" 
+            alt="dark background with rectangular blocks (fundo escuro com blocos retangulares)" 
+            className="w-full h-[340px] md:h-[500px] md:object-cover -z-20 absolute top-0 left-0" />
+    )
+}
