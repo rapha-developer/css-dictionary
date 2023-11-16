2cc158f2c318952015c80e193ac6ae35acfb3ff6
Rapha Developer <contatosraphaelmoreira@gmail.com>
2023-11-16 12:35:34 -0300

    [app] create new route for imported HomeProperty page in App Component

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './App.css'
 import { HomePage } from './components/home/HomePage'
 import { Sidebar } from './components/sidebar'
 import { HomeCourses } from './components/home/HomeCourses'
+import { HomeProperty } from './components/home/HomeProperty'
 
 function App() {
 
@@ -13,6 +14,7 @@ function App() {
       <Routes>
         <Route path='/' element={<HomePage />} />
         <Route path='/collection/:letter' element={<HomeCourses />} />
+        <Route path='/property/:propertyName' element={<HomeProperty />} />
         <Route path='*' element={<Navigate to="/" />} />
       </Routes> 
     </div>
