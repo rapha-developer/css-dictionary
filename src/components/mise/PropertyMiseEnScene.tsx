01878d9ddc9e34369a6be86b5edca840e40c7ce0
Rapha Developer <contatosraphaelmoreira@gmail.com>
2023-11-16 12:25:18 -0300

    [mise] create PropertyMiseEnScene component. Responsible for show background image from HomeProperty page

--- src/components/mise/PropertyMiseEnScene.tsx
+++ src/components/mise/PropertyMiseEnScene.tsx
@@ -3,7 +3,7 @@ export const PropertyMiseEnScene = () => {
         <div className="w-full h-full relative">
             <img src="/beautiful-sunset.jpg" 
             alt="a large sunset over the desert drawn (um grande por do sol ao longo do deserto desenhado)" 
-            className="w-full h-[310px] md:h-[720px] xl:h-[820px] object-cover -z-10 absolute top-20 md:top-0 left-0 opacity-40" />
+            className="w-full h-[310px] md:h-[720px] xl:h-[820px] object-cover -z-10 absolute top-20 md:top-0 left-0 opacity-40 md:opacity-70" />
             <div className="w-full h-[960px] -z-[9] absolute top-0 left-0 dark-gradient-categories"></div>
         </div>
     )
