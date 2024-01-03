b6084fc5977607075554dd8f9cd8661a155552d7
Rapha Developer <contatosraphaelmoreira@gmail.com>
2024-01-03 11:43:07 -0300

    data: export const HorizontalBar with horizontal bar buttons text

---
+++ src/data/HorizontalBarData.tsx
@@ -0,0 +1,17 @@
+
+
+export const HorizontalBarData = [
+    {
+        url: "#",
+        text: "description"
+    },
+    {
+        url: "#",
+        text: "examples"
+
+    },
+    {
+        url: "#",
+        text: "valores"
+    },
+]
