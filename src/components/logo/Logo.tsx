462d78a2c3531d812c0bae4892ea8b1e3f1827dd
Rapha Developer <contatosraphaelmoreira@gmail.com>
2024-01-17 16:41:43 -0300

    logo: create and export logo component to anyone who call it

---
+++ src/components/logo/Logo.tsx
@@ -0,0 +1,14 @@
+
+type Props = {
+    title: string
+}
+export const Logo = ({ title }: Props) => {
+    return (
+        <div className="logo flex gap-2 items-center">
+            <img src="/css-logo-icon.svg" 
+                alt="Academic Cap" 
+                className="w-10 h-10" />
+            <p className="font-outfit text-base font-semibold text-inherit">{title}</p>
+        </div>
+    )
+}
