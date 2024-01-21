406acce82e952312ae47c0c733f3303214f6e35c
Rapha Developer <contatosraphaelmoreira@gmail.com>
2024-01-21 18:00:40 -0300

    data: create and export Header Items Data constant, with all items to build Header

---
+++ src/data/HeaderItemsData.tsx
@@ -0,0 +1,29 @@
+
+type HeaderItem = {
+    url: string
+    text: string
+    outline: boolean
+}
+
+export const HeaderItemsData: HeaderItem[] =  [
+    {
+        url: "/",
+        text: "home",
+        outline: false
+    },
+    {
+        url: "/courses",
+        text: "courses",
+        outline: false
+    },
+    {
+        url: "/glossary",
+        text: "glossary",
+        outline: false
+    },
+    {
+        url: "/about",
+        text: "about",
+        outline: true
+    }
+];
