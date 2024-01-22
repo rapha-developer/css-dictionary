7c09a97e9472f1c9513e8cf4857ab1bca798727c
Rapha Developer <contatosraphaelmoreira@gmail.com>
2024-01-22 11:45:21 -0300

    header: import HeaderFirstSection, HeaderBtn and HeaderBtnOutline, use them inside the header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,8 @@
 import { useEffect, useState } from "react"
-import { Logo } from "../logo"
-import { BarIcon, CancelIcon } from "../../assets/icons"
 import { HeaderItemsData } from "../../data";
+import { HeaderFirstSection } from "./HeaderFirstSection";
+import { HeaderBtn } from "./HeaderBtn";
+import { HeaderBtnOutline } from "./HeaderBtnOutline";
 
 
 export const Header = () => {
@@ -27,27 +28,31 @@ export const Header = () => {
     return (
         <header className={`bg-white text-secondary ${headerClass} fixed top-0 left-0 pt-4 md:pt-0 w-full h-auto md:h-20 z-50 transition-colors`}>
             <div className="container mx-auto xl:max-w-[1400px] flex flex-col md:flex-row lg:gap-32 items-center justify-between h-full rounded-3xl md:px-2 md:pb-0 xl:px-0">
-                
-                <div className="w-full md:w-auto flex justify-between px-4 md:px-0">
-                    <Logo title="CSS Dictionary" />
-                    <button onClick={onHandlerHideHeader} className="md:hidden">
-                        {hideMenu && <BarIcon />}
-                        {!hideMenu && <CancelIcon />}
-                    </button>
-                </div>
+                <HeaderFirstSection 
+                    hideMenu={hideMenu}
+                    onHandler={onHandlerHideHeader}
+                />
                 <ul className={`nav-list flex flex-col md:flex-row gap-8 md:gap-10 items-center menu-hidden pb-3 md:pb-0`} aria-hidden={hideMenu} >
                     {Array.isArray(nav_menu) &&
                         nav_menu.length > 0 &&
                         nav_menu.map((navItem) => (
-                            <div key={navItem.text} className="relative group">
-                                <a
-                                    href={navItem.url} 
-                                    className="inline-block font-outfit text-base text-inherit font-medium capitalize">
-                                    {navItem.text}
-                                </a>
-                                <img src="/active-bar.svg" 
-                                    alt="Active bar" 
-                                    className={`hidden group-hover:block w-fit h-4 aspect-auto absolute left-0 top-4`} />
+                            <div className="header-item" key={navItem.text}>
+                                {(!navItem.outline) &&
+                                    <HeaderBtn 
+                                        key={navItem.text} 
+                                        text={navItem.text} 
+                                        url={navItem.url}
+                                        outline={navItem.outline}
+                                    />
+                                }
+                                {(navItem.outline) &&
+                                    <HeaderBtnOutline 
+                                        key={navItem.text} 
+                                        text={navItem.text} 
+                                        url={navItem.url}
+                                        outline={navItem.outline}
+                                    />
+                                }
                             </div>
                         ))
                     }
