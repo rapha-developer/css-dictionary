137a4e091f86ea37b43343dcb8173a62365ed83d
Rapha Developer <contatosraphaelmoreira@gmail.com>
2024-01-25 16:31:50 -0300

    data: export Hero Group Data constant, used in Hero section of the App

---
+++ src/data/HeroGroupData.tsx
@@ -0,0 +1,6 @@
+import { HeroGroupProps } from "../types/heroGroup";
+
+export const HeroGroupData: HeroGroupProps =  {
+    subtitle: "100% quality courses",
+    title: "Find your perfect courses and improve your skill"
+}
