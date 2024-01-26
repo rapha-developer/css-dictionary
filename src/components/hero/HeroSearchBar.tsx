afc42808fe328ea6720bd44a491ca0355f4fed7c
Rapha Developer <contatosraphaelmoreira@gmail.com>
2024-01-26 12:04:05 -0300

    hero: import debounce function, responsible to get value of input only after 500 miliseconds after user stops writing

--- src/components/hero/HeroSearchBar.tsx
+++ src/components/hero/HeroSearchBar.tsx
@@ -1,6 +1,14 @@
+import { ChangeEvent, useCallback } from "react"
+import _debounce from 'lodash/debounce'
 import { MagnifyGlassIcon } from "../../assets/icons"
 
 export const HeroSearchBar = () => {
+    const onChangeHandler = useCallback(
+        _debounce((event: ChangeEvent<HTMLInputElement>) => {
+            console.log({query: event.target.value })
+        }, 500),
+        []
+    )
     return (
         <div className="mt-2 flex bg-white rounded-full">
             <div className="h-auto px-6 rounded-full bg-icon-widget-gold flex items-center">
@@ -8,6 +16,7 @@ export const HeroSearchBar = () => {
             </div>
             <input type="search" 
                 placeholder="Search css properties here..."
+                onChange={onChangeHandler}
                 className="py-[14px] px-4 bg-white h-12 w-11/12 font-outfit text-sm font-medium rounded-full indent-2 focus-visible:outline-none"
             />
         </div>
