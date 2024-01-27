4cec9cf989dcce8096014c30f66134c28d321a2d
Rapha Developer <contatosraphaelmoreira@gmail.com>
2024-01-27 17:45:35 -0300

    hero: add background with position absolute and make entire hero section with height auto

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -2,12 +2,12 @@ import { HeroGroupData } from "../../data"
 import { WidgetHeroContainer } from "../widgets"
 import { HeroGroup } from "./HeroGroup"
 
-
 export const Hero = () => {
     const { title, subtitle } = HeroGroupData
     return (
-        <div className="background-hero w-full h-[665px]">
-            <div className="hero-content container mx-auto px-4 lg:px-8 pt-24 md:pt-56 pb-32 relative">
+        <div className="bg-transparent w-full h-auto">
+            <div className="background-hero absolute top-0 left-0 w-full h-[665px] -z-50"></div>
+            <div className="hero-content container mx-auto px-4 lg:px-8 pt-24 md:pt-56 relative">
                 <div className="flex flex-col gap-10 md:flex-row md:gap-0 flex-grow justify-around relative z-10">
                     <HeroGroup 
                         title={title}
