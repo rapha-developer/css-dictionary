bcbabb5f9da38f42df4c885a9208271b82641a71
Rapha Developer <contatosraphaelmoreira@gmail.com>
2024-01-29 11:33:53 -0300

    build: export HomeCoursesData and HomeCoursesHeadData constants, used them in Courses component

---
+++ src/data/HomeCoursesData.tsx
@@ -0,0 +1,68 @@
+import { CourseCard } from "../types/courseCard";
+
+export const HomeCoursesHeadData = {
+    subtitle: "featured courses",
+    title: "find yours from the featured",
+    description: {
+        first_line: "When An Unknown Printer Took A Galley Of Type And Scrambled It To Make A Type Specimen",
+        second_line: "Book It Has Survived Not Only Five Centuries"
+    }
+}
+
+export const HomeCoursesData: CourseCard[] = [
+    {
+        image: "https://www.radiustheme.com/demo/wordpress/themes/quiklearn/wp-content/uploads/2023/07/crs_img_2-380x277.jpg",
+        category: "technology",
+        title: "the complete android java developer course",
+        url: "#",
+        author: {
+            gravatar: "https://avatars.githubusercontent.com/u/91702283?v=4",
+            name: "raphael moreira"
+        },
+        price: "free"
+    },
+    {
+        image: "https://www.radiustheme.com/demo/wordpress/themes/quiklearn/wp-content/uploads/2023/07/crs_img_1-380x277.jpg",
+        category: "development",
+        title: "the complete android development course",
+        url: "#",
+        author: {
+            gravatar: "https://avatars.githubusercontent.com/u/91702283?v=4",
+            name: "raphael moreira"
+        },
+        price: "free"
+    },
+    {
+        image: "https://www.radiustheme.com/demo/wordpress/themes/quiklearn/wp-content/uploads/2023/07/crs_img_3-380x277.jpg",
+        category: "technology",
+        title: "the complete wordPress development course",
+        url: "#",
+        author: {
+            gravatar: "https://avatars.githubusercontent.com/u/91702283?v=4",
+            name: "raphael moreira"
+        },
+        price: "free"
+    },
+    {
+        image: "https://www.radiustheme.com/demo/wordpress/themes/quiklearn/wp-content/uploads/2023/07/crs_img_5-380x277.jpg",
+        category: "technology",
+        title: "the complete react native course",
+        url: "#",
+        author: {
+            gravatar: "https://avatars.githubusercontent.com/u/91702283?v=4",
+            name: "raphael moreira"
+        },
+        price: "free"
+    },
+    {
+        image: "https://www.radiustheme.com/demo/wordpress/themes/quiklearn/wp-content/uploads/2023/07/crs_img_4-380x277.jpg",
+        category: "technology",
+        title: "JQuery and JavaScript fundamental course",
+        url: "#",
+        author: {
+            gravatar: "https://avatars.githubusercontent.com/u/91702283?v=4",
+            name: "raphael moreira"
+        },
+        price: "free"
+    },
+]
