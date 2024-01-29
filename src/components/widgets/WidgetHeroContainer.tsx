3ea12470e83ac304180c976ebaf9bd2c51b6efb5
Rapha Developer <contatosraphaelmoreira@gmail.com>
2024-01-29 11:19:17 -0300

    fix: change width and margin properties

--- src/components/widgets/WidgetHeroContainer.tsx
+++ src/components/widgets/WidgetHeroContainer.tsx
@@ -5,7 +5,7 @@ export const WidgetHeroContainer = () => {
     
     const widgetList = WidgetListData
     return (
-        <div className="w-full bg-white py-7 px-3 sm:px-7 flex flex-wrap justify-center gap-6 -mt-20 sm:-mt-32 md:-mt-20 rounded-2mxl box-shadow-widget relative z-20">
+        <div className="w-fit mx-auto bg-white py-7 px-3 sm:px-7 flex flex-wrap justify-center gap-6 -mt-20 sm:-mt-32 md:-mt-20 rounded-2mxl box-shadow-widget relative z-20">
             {Array.isArray(widgetList) &&
             widgetList.length > 0 &&
             widgetList.map((widgetItem) => (
