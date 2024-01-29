a5f4837115459489f09c654196800bfbb3c5deef
Rapha Developer <contatosraphaelmoreira@gmail.com>
2024-01-29 11:31:05 -0300

    build: export courseCard type, used in coursesCard component

---
+++ src/types/courseCard.ts
@@ -0,0 +1,11 @@
+export type CourseCard = {
+    image: string
+    category: string
+    title: string
+    url: string
+    author: {
+        gravatar: string
+        name: string
+    },
+    price: "free" | "paid"
+}
