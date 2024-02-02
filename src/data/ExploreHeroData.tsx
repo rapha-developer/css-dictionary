3d2504615fe0b2acb3a1853102f37de6fa63bb82
Rapha Developer <contatosraphaelmoreira@gmail.com>
2024-02-02 12:21:54 -0300

    data: export explore hero data, with all information to be used in explore hero component

---
+++ src/data/ExploreHeroData.tsx
@@ -0,0 +1,12 @@
+import { exploreHeroGroup } from "../types/exploreHeroGroup"
+
+export const ExploreHeroData:  exploreHeroGroup =  {
+    subtitle: "learners and students",
+    title: "We teach the fundamentals of art while encouraging creativity, curiosity, and individuality.",
+    text: "when an unknown printer took a galley offer area type and scrambled to make a type specimen book has survived when an unknown printer took a galley offer area type and scrambled to make.",
+    button: {
+        url: "#",
+        text: "explore all courses",
+        signal: " ➔"
+    }
+}
