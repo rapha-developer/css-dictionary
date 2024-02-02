1ba9b71076fe17889c0a4df1c18bc54a83c5bf97
Rapha Developer <contatosraphaelmoreira@gmail.com>
2024-02-02 12:41:40 -0300

    data: export recommended websites data, with all icons e alternatives texts used in recommended websites (component)

---
+++ src/data/RecommendedWebsiteData.tsx
@@ -0,0 +1,31 @@
+
+type RecommendedWebsites = {
+    icon: string
+    alt: string
+}
+export const RecommendedWebsiteData: RecommendedWebsites[] =  [
+    {
+        icon: "img13",
+        alt: "W3Schools"
+    },
+    {
+        icon: "img14",
+        alt: "MDN Web Docs"
+    },
+    {
+        icon: "img15",
+        alt: "CSS Tricks"
+    },
+    {
+        icon: "img16",
+        alt: "CSS Free Code Camp"
+    },
+    {
+        icon: "img17",
+        alt: "CSS Scrimba"
+    },
+    {
+        icon: "img18",
+        alt: "CSS  Stack Overflow"
+    }
+]
