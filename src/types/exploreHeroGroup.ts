4b5e1f484dedf6387cb20ccf54dfdf54493ae81e
Rapha Developer <contatosraphaelmoreira@gmail.com>
2024-02-02 12:08:54 -0300

    type: export explore Hero Group type, used in explore hero data

---
+++ src/types/exploreHeroGroup.ts
@@ -0,0 +1,10 @@
+export type exploreHeroGroup = {
+    subtitle: string
+    title: string
+    text: string
+    button: {
+        url: string
+        text: string
+        signal: string
+    }
+}
