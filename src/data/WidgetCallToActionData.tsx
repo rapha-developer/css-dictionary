a7856dc864bd55d7bad4d10c1ec60222b8760f6f
Rapha Developer <contatosraphaelmoreira@gmail.com>
2024-02-05 11:47:07 -0300

    data: export Widget call to action data, with all information to fill widget call to action component

---
+++ src/data/WidgetCallToActionData.tsx
@@ -0,0 +1,9 @@
+import { widgetToActionProps } from "../types/widgetToActionProps"
+
+export const WidgetCallToActionData: widgetToActionProps = {
+    title: "affordable online courses & learning opportunities for you",
+    button: {
+        url: "#",
+        text: "start learning today"
+    }
+}
