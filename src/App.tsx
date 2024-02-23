cda479af38ff12e8cf8b2bb399917a49a7afa756
Rapha Developer <contatosraphaelmoreira@gmail.com>
2024-02-23 18:05:43 -0300

    feat: import and use browserRoute and other props from react router dom, to make all routes necessary in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
+import { useAtom } from 'jotai'
 import './App.css'
+import { queryAtom } from './atoms'
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
+import { HomePage } from './components/pages/HomePage'
+import { CoursesPage } from './components/pages/CoursesPage'
 
 function App() {
-
+  const [query] = useAtom(queryAtom) 
+  
   return (
-    <>
-      <h1 className="font-mono text-3xl text-blue-800">Tailwind is work with Vite</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/courses' element={<CoursesPage />} />
+        <Route path='*' element={<Navigate to="/" />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
