a66c844ed0a9b2f44d123ef56c500cf8f2953317
Rapha Developer <contatosraphaelmoreira@gmail.com>
2024-03-06 18:41:03 -0300

    icon: add Bread Crumb Symbol Icon, used in Widget Bread Crumbs component

---
+++ src/assets/icons/BreadCrumbSymbolIcon.tsx
@@ -0,0 +1,9 @@
+
+
+export const BreadCrumbSymbolIcon = () => {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" version="1.1" className="w-5 h-5 mt-[1px] text-white" id="Layer_1" x="0px" y="0px" width="20px" height="20px" viewBox="0 0 16 16" enableBackground="new 0 0 16 16" xmlSpace="preserve">
+            <path fill="#FFFFFF" d="M6,13c-0.3,0-0.5-0.1-0.7-0.3c-0.4-0.4-0.4-1,0-1.4L8.6,8L5.3,4.7c-0.4-0.4-0.4-1,0-1.4s1-0.4,1.4,0l4,4  c0.4,0.4,0.4,1,0,1.4l-4,4C6.5,12.9,6.3,13,6,13z"/>
+        </svg>
+    )
+}
