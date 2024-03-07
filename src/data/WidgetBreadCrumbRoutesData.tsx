8191f1db1207438d78b70a2fd35a02f899fcfb61
Rapha Developer <contatosraphaelmoreira@gmail.com>
2024-03-07 10:43:44 -0300

    data: export (bread crumbs courses route) const, used to fill widget bread crumbs component

---
+++ src/data/WidgetBreadCrumbRoutesData.tsx
@@ -0,0 +1,15 @@
+import { widgetBreadCrumb } from "../types/widgetBreadCrumb";
+
+
+export const BreadCrumbCoursesRoute: widgetBreadCrumb =  {
+    historic: [
+        {
+            url: "/",
+            text: "home"
+        }
+    ],
+    actual: {
+        url: "/courses",
+        text: "courses"
+    }
+}
