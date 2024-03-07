c2041b5cc114a0cfa7350136b766f0c28b9c6251
Rapha Developer <contatosraphaelmoreira@gmail.com>
2024-03-07 11:01:14 -0300

    feat: export (heading title) component

---
+++ src/components/heading/HeadingTitle.tsx
@@ -0,0 +1,8 @@
+
+
+export const HeadingTitle = ( { text }: { text: string } ) => {
+
+    return (
+        <h1 className="font-outfit text-4xl md:text-5xl text-white font-semibold capitalize">{text}</h1>
+    )
+}
