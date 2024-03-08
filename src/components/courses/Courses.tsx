daf8ecfe23d1317d421a41e6d2180ff6c80125ac
Rapha Developer <contatosraphaelmoreira@gmail.com>
2024-03-08 19:44:06 -0300

    refactor: change Courses component to show versions depending on whether it is home page or not

--- src/components/courses/Courses.tsx
+++ src/components/courses/Courses.tsx
@@ -1,24 +1,32 @@
 
+import { useLocation } from "react-router-dom"
 import { HomeCoursesHeadData } from "../../data/HomeCoursesData"
 import { CoursesGroupCard } from "./CoursesGroupCard"
+import { CoursesHero } from "./CoursesHero"
+import { CoursesLessonsHero } from "./CoursesLessonsHero"
+import { CoursesLessonsGroupCard } from "./CoursesLessonsGroupCard"
 
 export const Courses = () => {
     const coursesHeadData = HomeCoursesHeadData
+    const actualLocation = useLocation()
+    const isHomePage = (actualLocation.pathname === '/') ? true : false
     return (
         <section className="section-courses text-body-200 py-32">
             <div className="container mx-auto flex flex-col justify-center">
-                <div className="courses-head">
-                    <p className="font-outfit text-sm font-medium text-body-100 tracking-[.6px] uppercase text-center mb-2">{coursesHeadData.subtitle}</p>
-                    <h2 className="font-outfit text-4xl font-semibold text-secondary capitalize text-center mb-0.5">{coursesHeadData.title}</h2>
-                    <img src="/active-bar.svg" 
-                        alt="Yellow line" 
-                        className="w-[167px] h-auto mx-auto mb-5" />
-                    <p className="font-outfit text-base font-normal text-body-100 leading-7 capitalize text-center">
-                        {coursesHeadData.description.first_line}<br />
-                        {coursesHeadData.description.second_line}
-                    </p>
-                </div>
-                <CoursesGroupCard />
+                {isHomePage && 
+                    <CoursesHero 
+                        title={coursesHeadData.title}
+                        subtitle={coursesHeadData.subtitle}
+                        description={coursesHeadData.description}
+                    />
+                }
+                {isHomePage && <CoursesGroupCard />}
+                {!isHomePage && 
+                <CoursesLessonsHero 
+                    title={coursesHeadData.title}
+                    subtitle={coursesHeadData.subtitle}
+                />}
+                {!isHomePage && <CoursesLessonsGroupCard />}
             </div>
         </section>
     )
