fa8f7f646015b8ce7a6cc5b991f15c4f1e975215
Rapha Developer <contatosraphaelmoreira@gmail.com>
2024-03-14 11:08:28 -0300

    refactor: update image positions to be responsive on small screens

--- src/components/heading/BackgroundHeading.tsx
+++ src/components/heading/BackgroundHeading.tsx
@@ -11,10 +11,10 @@ export const BackgroundHeading = () => {
                 className="w-full absolute -bottom-1 left-0 -z-40" />
             <img src="/hero-books.png" 
                 alt="Hero Books" 
-                className="absolute bottom-20 md:bottom-40 right-16 w-36 md:w-56 -z-40" />
+                className="absolute bottom-14 sm:bottom-20 md:bottom-40 right-8 sm:right-16 w-36 md:w-56 -z-40" />
             <img src="/hero-img-4.svg" 
-                alt="Hero" 
-                className="absolute bottom-32 sm:bottom-20 md:bottom-40 left-10 sm:left-16 w-16 md:w-[114px] -z-40" />
+                alt="Hero half sphere (meia esfera)" 
+                className="absolute bottom-20 sm:bottom-20 md:bottom-40 left-10 sm:left-16 w-16 md:w-[114px] -z-40" />
         </>
     )
 }
