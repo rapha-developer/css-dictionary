d1b316e3d98778e18ba0667671bacb4dd5f08a69
Rapha Developer <contatosraphaelmoreira@gmail.com>
2024-03-16 10:17:31 -0300

    type: export Lessons Hero type, used in Lessons Course Hero component

---
+++ src/types/lessonsHero.ts
@@ -0,0 +1,10 @@
+export type LessonsHero = {
+    category: string
+    title: string
+    description: string
+    author: {
+        gravatar: string
+        name: string
+    },
+    lastUpdate: string
+}
