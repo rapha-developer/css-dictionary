44c27e587771e157a1f06fe6fceff07b36f4a120
Rapha Developer <contatosraphaelmoreira@gmail.com>
2024-03-16 10:21:30 -0300

    data: export (lessons hero data) const, used to fill lessons course hero component

---
+++ src/data/LessonsHeroData.tsx
@@ -0,0 +1,13 @@
+import { LessonsHero } from "../types/lessonsHero";
+
+
+export const LessonsHeroData: LessonsHero = {
+    category: "technology",
+    title: "the complete android java developer course",
+    description: "in this course, you’ll learn android development and get to build your own android t apps by using java. apply for your area factory dream job as an android developer",
+    author: {
+        gravatar: "https://avatars.githubusercontent.com/u/91702283?v=4",
+        name: "raphael moreira"
+    },
+    lastUpdate: "8 months ago"
+}
