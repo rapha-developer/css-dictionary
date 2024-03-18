2a76c32cfbf9d3bff8ed4ac7627e89568877e49e
Rapha Developer <contatosraphaelmoreira@gmail.com>
2024-03-18 10:44:31 -0300

    data: export (widget accordion list data) const, used to fill widget accordion component

---
+++ src/data/WidgetAccordionListData.tsx
@@ -0,0 +1,17 @@
+import { WidgetAccordionElement } from "../types/widgetAccordionGroup";
+
+
+export const WidgetAccordionListData: WidgetAccordionElement[] = [
+    {
+        label: "lecture 1.1",
+        description: "course installation 04"
+    },
+    {
+        label: "lecture 1.2",
+        description: "course installation 05"
+    },
+    {
+        label: "lecture 1.3",
+        description: "course installation 06"
+    },
+];
