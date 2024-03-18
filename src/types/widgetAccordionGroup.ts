f4a77576129ac0b25967edd882c07f746a6aa3cb
Rapha Developer <contatosraphaelmoreira@gmail.com>
2024-03-18 10:21:08 -0300

    type: export (widget acccordion group) type, used in widget accordion component

---
+++ src/types/widgetAccordionGroup.ts
@@ -0,0 +1,10 @@
+
+export type WidgetAccordionElement = {
+    label: string
+    description: string
+}
+export type WidgetAccordionList = {
+    heading: string
+    numberLessons: string
+    items: WidgetAccordionElement[]
+}
