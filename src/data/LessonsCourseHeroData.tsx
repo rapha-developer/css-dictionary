3254ebe78924192196a628bb2dd894ad37a53006
Rapha Developer <contatosraphaelmoreira@gmail.com>
2024-03-18 10:46:45 -0300

    data: export (lessons course hero data) const, used to fill lessons full card component

---
+++ src/data/LessonsCourseHeroData.tsx
@@ -0,0 +1,19 @@
+import { LessonsCourseCard } from "../types/lessonAreaCard";
+
+export const LessonsCourseHeroData: LessonsCourseCard = {
+    heading: "this course includes: ",
+    items: [
+        {
+            label: "level",
+            value: "beginner"
+        },
+        {
+            label: "lessons",
+            value: "8"
+        },
+        {
+            label: "language",
+            value: "english"
+        }
+    ]
+}
