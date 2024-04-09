c9f2371fefa481ec24ecb9036f227c982f5eb926
Rapha Developer <contatosraphaelmoreira@gmail.com>
2024-04-09 18:26:36 -0300

    feat: change lessonsPage and lecturePage routes

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 import { HomePage } from './components/pages/HomePage'
 import { CoursesPage } from './components/pages/CoursesPage'
 import { LessonsPage } from './components/pages/LessonsPage'
+import { LecturePage } from './components/pages/LecturePage'
 
 function App() {
   const [query] = useAtom(queryAtom) 
@@ -14,7 +15,8 @@ function App() {
       <Routes>
         <Route path='/' element={<HomePage />} />
         <Route path='/courses' element={<CoursesPage />} />
-        <Route path='/courses/*' element={<LessonsPage />} />
+        <Route path='/courses/:courseID' element={<LessonsPage />} />
+        <Route path='/courses/lectures/:lectureID' element={<LecturePage />} />
         <Route path='*' element={<Navigate to="/" />} />
       </Routes>
     </BrowserRouter>
