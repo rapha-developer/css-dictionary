bb6e6bc00268c2d4a55b695513f839300773451b
Rapha Developer <contatosraphaelmoreira@gmail.com>
2024-06-05 10:01:50 -0300

    feat: export const (property with a types); responsible for storing all css properties that start with letter A

---
+++ src/lecture/propertyList/propertyListBeginWithA.ts
@@ -0,0 +1,8 @@
+import { AccentColor } from "../../propertyData/a/AccentColor";
+import { AlignContent } from "../../propertyData/a/AlignContent";
+import { propertyGenericType } from "../../types/propertyGenericType";
+
+export const propertyWithATypes: propertyGenericType = {
+    accentcolor: AccentColor,
+    aligncontent: AlignContent
+}
