dcacfbd15cd196ac8a9fcdfa6543a722b58671c1
Rapha Developer <contatosraphaelmoreira@gmail.com>
2024-06-05 09:43:58 -0300

    refactor: create (params to build route types) to assing output formatting: firstLetter, propertyClassName, propertyName

--- src/utils/paramsToBuildRoute.tsx
+++ src/utils/paramsToBuildRoute.tsx
@@ -1,18 +1,25 @@
 import { paramsToLazyRoutes } from "../types/paramsToLazyRoutes"
 
-export function paramsToBuildRoute(params: paramsToLazyRoutes) {
+type paramsToBuildRouteTypes = {
+    firstLetter: string
+    propertyClassName: string
+    propertyName: string
+}
+export function paramsToBuildRoute(params: paramsToLazyRoutes): paramsToBuildRouteTypes {
     if (params.lectureID?.charAt(0)) {
         const names = params.lectureID.split('-').map((item) => item.charAt(0).toLocaleUpperCase() + item.slice(1))
         const firstLetter = params.lectureID.charAt(0)
-        const componentName = names.reduce((acc, current) => acc + current, '')
-        
+        const propertyClassName = names.reduce((acc, current) => acc + current, '')
+        const propertyName = names.reduce((acc, current) => acc + ' ' + current, '')
         return {
             firstLetter: firstLetter,
-            componentName: componentName
+            propertyClassName: propertyClassName,
+            propertyName: propertyName
         }
     }
     return {
         firstLetter: 'u',
-        componentName: '404'
+        propertyClassName: '404',
+        propertyName: 'undefined' 
     }
 }
