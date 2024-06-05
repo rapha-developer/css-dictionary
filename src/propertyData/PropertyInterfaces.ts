b97183c5dea7dd49cc662a8ce45d9607c878b34a
Rapha Developer <contatosraphaelmoreira@gmail.com>
2024-06-05 09:48:10 -0300

    intefaces: export two interfaces: PropertyValue and PropertyExample. Responsible for responsible for ensuring what information on values and examples on the readings page should look like

---
+++ src/propertyData/PropertyInterfaces.ts
@@ -0,0 +1,11 @@
+
+export interface PropertyValue {
+    title: string
+    value: string
+}
+
+export interface PropertyExample {
+    exampleId: string
+    exampleTitle: string
+    exampleDescription: string
+}
