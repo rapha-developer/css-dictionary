ff8107d4f631ba564b8eff69964a1d1638c17df6
Rapha Developer <contatosraphaelmoreira@gmail.com>
2024-06-06 10:21:56 -0300

    refctor: import property data from method getPropertyInfo. And fill Lecture content with that information

--- src/components/lectures/Lectures.tsx
+++ src/components/lectures/Lectures.tsx
@@ -1,20 +1,26 @@
-import React, { Suspense } from "react"
-import { Loading } from "../loading/Loading.tsx"
 import { useParams } from "react-router-dom";
 import { paramsToLazyRoutes } from "../../types/paramsToLazyRoutes.ts";
 import { paramsToBuildRoute } from "../../utils/paramsToBuildRoute.tsx";
+import { RunPropertyInfoDynamically } from "../../lecture/RunPropertyDynamically.ts";
+import { LectureContent } from "./LectureContent.tsx";
 
 export const Lectures = () => {
     const params = useParams<paramsToLazyRoutes>()
-    const { firstLetter, componentName } = paramsToBuildRoute(params)    
-    const LectureContent = React.lazy(() => import(`../../lecture/${firstLetter}/${componentName}.tsx`));
+    const { firstLetter, propertyClassName, propertyName } = paramsToBuildRoute(params)    
+    const getPropertyInfoDynamically = new RunPropertyInfoDynamically(firstLetter)
+    const getSpecificProperty = new (getPropertyInfoDynamically.getProperty(propertyClassName))();
     
     return (
         <section className="bg-gradient-to-b from-body-200 from-10% via-white via-30% to-white to-90% pt-20 pb-32 h-fit">
             <div className="container mx-auto">
-                <Suspense fallback={<Loading />}>
-                    <LectureContent />
-                </Suspense>
+                {getSpecificProperty && 
+                    <LectureContent 
+                        name={propertyName} 
+                        definition={getSpecificProperty.showDefinition()}
+                        values={getSpecificProperty.showValues()}
+                        examples={getSpecificProperty.showExamples()}
+                    />
+                }
             </div>
         </section>
     )
