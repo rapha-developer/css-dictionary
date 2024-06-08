fb706a0ba89cecb8f3487f5475f80c5507a0ccaa
Rapha Developer <contatosraphaelmoreira@gmail.com>
2024-06-08 17:20:24 -0300

    create a new development environment made with Vite + Tailwind CSS

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+// import { useState } from 'react'
+import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className="font-sans text-2xl text-purple-700">
+        Te amo
+      </h1>
+    </>
+  )
+}
+
+export default App
