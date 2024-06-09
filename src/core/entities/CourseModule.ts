403fabb194f5108a77ed397f6d6aedbfb17068f1
Rapha Developer <contatosraphaelmoreira@gmail.com>
2024-06-09 11:38:47 -0300

    feat: export class CourseModule. Responsible for modeling modules from entity

---
+++ src/core/entities/CourseModule.ts
@@ -0,0 +1,34 @@
+import { v4 as uuidV4 } from "uuid"
+import { Property } from "./Property"
+
+export class CourseModule {
+    private _id: string
+    private _name: string
+    private _property: Property
+
+    constructor({
+        id,
+        name,
+        property
+    }: {
+        id: string
+        name: string
+        property: Property
+    }) {
+        this._id = id ?? uuidV4()
+        this._name = name
+        this._property = property
+    }
+
+    public get id(): string {
+        return this._id
+    }
+
+    public get name(): string {
+        return this._name
+    }
+
+    public get property(): Property {
+        return this._property
+    }
+}
