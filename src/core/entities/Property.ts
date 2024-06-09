b2ae6ccf7be3a100bb964bad7b9853abbedb0654
Rapha Developer <contatosraphaelmoreira@gmail.com>
2024-06-09 14:00:36 -0300

    refactor: import and use two types, one for examples and another for values. Create PropertyValue and PropertyExample inside the constructor. And change type return of values and examples methods

--- src/core/entities/Property.ts
+++ src/core/entities/Property.ts
@@ -1,6 +1,7 @@
 import { v4 as uuidV4 } from "uuid";
 import { PropertyValue } from "./PropertyValue";
 import { PropertyExample } from "./PropertyExample";
+import { PropertyExampleProps, PropertyValueProps } from "../../types/PropertyProps";
 
 export class Property {
     private _id: string;
@@ -16,17 +17,25 @@ export class Property {
         values,
         examples,
     }: {
-        id: string,
+        id?: string,
         name: string,
         category: "css property" | "css function" | string
-        values: PropertyValue[]
-        examples: PropertyExample[]
+        values: PropertyValueProps[]
+        examples: PropertyExampleProps[]
     }) {
         this._id = id ?? uuidV4()
         this._name = name
         this._category = category
-        this._values = values
-        this._examples = examples
+        this._values = values.map((item) => new PropertyValue({
+            id: item?.id,
+            name: item.name,
+            value: item.value
+        }))
+        this._examples = examples.map((example) => new PropertyExample({
+            id: example?.id,
+            name: example.name,
+            code: example.code
+        }))
     }
 
     public get id(): string {
@@ -41,11 +50,19 @@ export class Property {
         return this._category
     }
 
-    public get values(): PropertyValue[] {
+    public get values(): {
+        id?: string,
+        name: string,
+        value: string
+    }[] {
         return this._values
     }
 
-    public get examples(): PropertyExample[] {
+    public get examples(): {
+        id?: string
+        name: string
+        code: string
+    }[] {
         return this._examples
     }
 }
