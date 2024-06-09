cdb77f19f9a842bd6192f10d10cace68b976a0eb
Rapha Developer <contatosraphaelmoreira@gmail.com>
2024-06-09 11:05:46 -0300

    feat: export class PropertyExample. Responsible for modeling examples of the property entity

---
+++ src/core/entities/PropertyExample.ts
@@ -0,0 +1,34 @@
+import { v4 as uuidV4 } from "uuid"
+
+
+export class PropertyExample {
+    private _id: string
+    private _name: string
+    private _code: string
+
+    constructor({
+        id,
+        name,
+        code
+    }: {
+        id: string
+        name: string
+        code: string
+    }) {
+        this._id = id ?? uuidV4()
+        this._name = name
+        this._code = code
+    }
+
+    public get id(): string {
+        return this._id
+    }
+    
+    public get name(): string {
+        return this._name
+    }
+    
+    public get code(): string {
+        return this._code
+    }
+}
