b56a92e0774ebc830c758993770e189b1973c809
Rapha Developer <contatosraphaelmoreira@gmail.com>
2024-06-17 16:31:16 -0300

    icon: add new bar and cancel icons. Used in Header component

---
+++ src/assets/icons/CancelIcon.tsx
@@ -0,0 +1,9 @@
+
+
+export const CancelIcon = ({ classNames }: { classNames: string }) => {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={classNames}>
+            <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
+        </svg>
+    )
+}
