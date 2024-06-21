9cac3cf570d82cc7cb78c96086f59f65828d3868
Rapha Developer <contatosraphaelmoreira@gmail.com>
2024-06-21 12:26:17 -0300

    feat: export course page. Responsible for entire Course page

---
+++ src/components/pages/CoursePage.tsx
@@ -0,0 +1,44 @@
+import { useLocation, useNavigate } from "react-router-dom"
+import { CourseProps } from "../../types/CourseProps"
+import { useEffect, useState } from "react"
+import { CoursesInfo } from "../sections/CoursesInfo"
+import { Header } from "../header"
+import { Hero404 } from "../utils/Hero404"
+
+export const CoursePage = () => {
+    const navigate = useNavigate()
+    const { state } = useLocation()
+    const [courseState] = useState<CourseProps>(state?.course)
+    const courseExist = (courseState?.name) ? true : false
+    
+    useEffect(() => {
+        setTimeout(() => {
+            if(!courseState) {
+                return navigate("/")
+            }
+        }, 5000);
+    }, [courseState, navigate])
+
+    return (
+        <div className=""> 
+            <Header />
+            <img src="/hero-main.png" 
+                className="w-full max-h-[540px] object-cover"
+            />
+            {courseExist &&
+            <CoursesInfo 
+                id={courseState?.id}
+                name={courseState.name}
+                category={courseState.category}
+                price={courseState.price}
+                author={courseState.author}
+                modules={courseState.modules}
+                info={courseState.info}
+            />
+            }
+            {!courseExist &&
+            <Hero404 />
+            }
+        </div>
+    )
+}
