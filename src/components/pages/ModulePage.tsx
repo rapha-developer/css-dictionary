7f99a3577ebb829a5f602ff38c02e2fc92363eb9
Rapha Developer <contatosraphaelmoreira@gmail.com>
2024-06-21 12:24:36 -0300

    feat: export module page. Responsible for entire Module page

---
+++ src/components/pages/ModulePage.tsx
@@ -0,0 +1,42 @@
+import { useLocation, useNavigate } from "react-router-dom"
+import { Header } from "../header";
+import { PropertyProps } from "../../types/PropertyProps";
+import { useEffect, useState } from "react";
+import { PropertyBody } from "../sections/PropertyBody";
+import { Hero404 } from "../utils/Hero404";
+
+export const ModulePage = () => {
+    const navigate = useNavigate()
+    const { state } = useLocation()
+    const [propertyState] = useState<PropertyProps>(state?.property)
+    const propertyExist = (propertyState) ? true : false
+
+    useEffect(() => {
+        setTimeout(() => {
+            if(!propertyState) {
+                return navigate("/")
+            }
+        }, 5000);
+    }, [propertyState, navigate])
+
+    return (
+        <div className="">
+            <Header />
+            <img src="/hero-main.png" 
+                className="w-full max-h-[540px] object-cover"
+            />
+            {propertyExist &&
+            <PropertyBody 
+                name={propertyState.name}
+                definition={propertyState.definition}
+                category={propertyState.category}
+                values={propertyState.values}
+                examples={propertyState.examples}
+            />
+            }
+            {!propertyExist &&
+            <Hero404 />
+            }
+        </div>
+    )
+}
