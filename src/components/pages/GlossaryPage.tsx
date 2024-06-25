5a2d46e83cef86d9651ed296bf7d99cb7e667e01
Rapha Developer <contatosraphaelmoreira@gmail.com>
2024-06-24 21:17:43 -0300

    feat: export Glossary Page. Responsible for entire glossary page

---
+++ src/components/pages/GlossaryPage.tsx
@@ -0,0 +1,31 @@
+import { useAtom } from "jotai"
+import { Header } from "../header"
+import { GlossaryHero } from "../sections/GlossaryHero"
+import { glossaryControllerAtom } from "../../atoms/glossary"
+import { GlossaryViewModel } from "../../view-models/GlossaryViewModel"
+
+export const GlossaryPage = () => {
+    const [glossaryControllerData] = useAtom(glossaryControllerAtom)
+    const glossaries = glossaryControllerData.listGlossaries().map((glossary) => GlossaryViewModel.viewGlossary(glossary))
+
+    return (
+        <div className="">
+            <Header />
+            <img src="/hero-main.png" 
+                className="w-full max-h-[540px] object-cover"
+            />
+            <div className="bg-gradient-to-b from-body-200 from-10% via-white via-30% to-white to-90% h-auto w-full">
+            {Array.isArray(glossaries) &&
+            glossaries.length > 0 &&
+            glossaries.map((glossary) => (
+                <GlossaryHero 
+                    key={glossary.id}
+                    name={glossary.name}
+                    terms={glossary.terms}
+                />
+            ))
+            }
+            </div>
+        </div>
+    )
+}
