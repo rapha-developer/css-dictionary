30370884326616b76ba3ad1bebe7bfaa6e82b802
Rapha Developer <contatosraphaelmoreira@gmail.com>
2024-06-30 16:20:15 -0300

    feat: delete the old alignItems constant. And export alignItemsData from new file inside the data folder

---
+++ src/data/mocks/properties/a/data/alignItems.ts
@@ -0,0 +1,83 @@
+import { PropertyProps } from "../../../../../types/PropertyProps";
+
+export const alignItemsData: PropertyProps = {
+    name: "align items",
+    definition: "A propriedade align-items especifica o alinhamento padrão para itens dentro de um flexbox ou contêiner grid",
+    category: "css property",
+    values: [
+        {
+            name: "normal",
+            value: "Comporta-se como 'stretch' para flexbox e itens grid, ou 'start' para itens grid com um tamanho de bloco definido."
+        },
+        {
+            name: "stretch",
+            value: "Os itens são esticados para caber no contêiner"
+        },
+        {
+            name: "center",
+            value: "Os itens são posicionados no centro do contêiner"
+        },
+        {
+            name: "flex-start",
+            value: "Os itens são posicionados no início do contêiner"
+        },
+        {
+            name: "flex-end",
+            value: "Os itens são posicionados no final do contêiner"
+        },
+        {
+            name: "start",
+            value: "Os itens são posicionados no início de suas células individuais do grid, na direção do bloco"
+        },
+        {
+            name: "end",
+            value: "Os itens são posicionados no final de suas células individuais do grid, na direção do bloco"
+        },
+        {
+            name: "baseline",
+            value: "Os itens são posicionados na linha de base do contêiner"
+        },
+        {
+            name: "initial",
+            value: "Define esta propriedade com seu valor padrão. Leia sobre inicial"
+        },
+        {
+            name: "inherit",
+            value: "Herda esta propriedade de seu elemento pai. Leia sobre herdar"
+        },
+    ],
+    examples: [
+        {
+            name: "normal",
+            code: `normal`
+        },
+        {
+            name: "stretch",
+            code: `stretch`
+        },
+        {
+            name: "center",
+            code: `center`
+        },
+        {
+            name: "flex-start",
+            code: `flex-start`
+        },
+        {
+            name: "flex-end",
+            code: `flex-end`
+        },
+        {
+            name: "baseline",
+            code: `baseline`
+        },
+        {
+            name: "initial",
+            code: `initial`
+        },
+        {
+            name: "inherit",
+            code: `inherit`
+        },
+    ]
+} 
