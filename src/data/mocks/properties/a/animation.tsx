2f707ada23e3cfc112392644d81fd63291cbeeb4
Rapha Developer <contatosraphaelmoreira@gmail.com>
2024-07-02 18:17:20 -0300

    refactor: update function props to receive name & value. Import and use CopyAndPasteTemplate component.

--- src/data/mocks/properties/a/animation.tsx
+++ src/data/mocks/properties/a/animation.tsx
@@ -1,9 +1,17 @@
+import { CopyAndPasteTemplate } from "../../../../components/example/CopyAndPasteTemplate"
+import { PropertyTemplateProps } from "../../../../types/PropertyTemplateProps"
 
-export default function AnimationTemplate({ property }: { property: string }) {
-    const classNames = `h-12 w-12 rounded-md background-hero flex justify-center items-center animate-${property}`
+export default function AnimationTemplate({ name, value }: PropertyTemplateProps['property']) {
+    const classNames = `mx-auto h-12 w-12 rounded-md background-hero flex justify-center items-center animate-${value}`
     return (
-        <div className={classNames}>
-            <span className="font-outfit text-2xl font-semibold text-center text-body-200">01</span>
+        <div className="flex flex-col gap-4">
+            <div className={classNames}>
+                <span className="font-outfit text-2xl font-semibold text-center text-body-200">01</span>
+            </div>
+            <CopyAndPasteTemplate 
+                name={name}
+                value={value}
+            />
         </div>
     )
 } 
