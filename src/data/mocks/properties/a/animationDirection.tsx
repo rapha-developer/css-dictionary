0c3d6a7de98edd388fab8401b58f28ca9355798c
Rapha Developer <contatosraphaelmoreira@gmail.com>
2024-07-02 18:21:00 -0300

    refactor: update function props to receive name & value. Import and use inheritTemplate and CopyAndPasteTemplate components. Remove unnecessary code

--- src/data/mocks/properties/a/animationDirection.tsx
+++ src/data/mocks/properties/a/animationDirection.tsx
@@ -1,17 +1,23 @@
+import { CopyAndPasteTemplate } from "../../../../components/example/CopyAndPasteTemplate";
+import { InheritTemplate } from "../../../../components/example/InheritTemplate";
+import { PropertyTemplateProps } from "../../../../types/PropertyTemplateProps";
 
-export default function AnimationDirectionTemplate({ property }: {property: string}) {
+export default function AnimationDirectionTemplate({ name, value }: PropertyTemplateProps['property']) {
+    const inheritValue = "reverse"
     return (
-        <div className="flex flex-col gap-4" style={{animationDirection: "reverse"}}>
+        <div className="flex flex-col gap-4" style={{animationDirection: inheritValue}}>
             <div className={`h-16 w-16 rounded-md background-hero mx-auto flex justify-center items-center animate-spin`}
-            style={{animationDirection: property}}>
+            style={{animationDirection: value}}>
                 <span className="font-outfit text-2xl font-semibold text-center text-body-200">01</span>
             </div>
-            <p  className="font-outfit text-secondary text-base text-center">Elemento pai tem animation-direction como&nbsp;
-                <span className="inline-block bg-yellow-200 px-2 rounded-md text-secondary font-outfit text-sm font-bold">reverse</span>
-            </p>
-            <p className="font-outfit text-secondary text-sm font-medium text-center bg-borderLine py-3 px-4 rounded-md w-fit mt-4 mx-auto">
-                animation-direction: {property}
-            </p>
+            <InheritTemplate 
+                property={{name: name, value: value}}
+                inheritValue={inheritValue}
+            />
+            <CopyAndPasteTemplate 
+                name={name}
+                value={value}
+            />
         </div>
     )
 }
