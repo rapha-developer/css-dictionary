37715b1a2bb0edb9889769624cd22c027fc169bc
Rapha Developer <contatosraphaelmoreira@gmail.com>
2024-07-02 12:04:55 -0300

    refactor: update time value. And add initial value & inherit value in values and examples

--- src/data/mocks/properties/a/data/animationDelay.ts
+++ src/data/mocks/properties/a/data/animationDelay.ts
@@ -7,8 +7,16 @@ export const animationDelayData: PropertyProps = {
     values: [
         {
             name: "tempo",
-            value: "O deslocamento de tempo, do momento em que a animação é aplicada ao elemento, no qual a animação deve começar. Isso pode ser especificado em segundos ( s) ou milissegundos ( ms). A unidade é obrigatória."
-        }
+            value: "Define o número de segundos(s) ou milissegundos(ms) de espera antes do início da animação. O valor padrão é 0. Valores negativos são permitidos. Se você usar valores negativos, a animação começará como se já tivesse sido reproduzida por N segundos/milissegundos."
+        },
+        {
+            name: "initial",
+            value: "Define esta propriedade com seu valor padrão. Leia sobre initial no glossário"
+        },
+        {
+            name: "inherit",
+            value: "Herda esta propriedade de seu elemento pai. Leia sobre inherit no glossário"
+        },
     ],
     examples: [
         {
@@ -22,6 +30,14 @@ export const animationDelayData: PropertyProps = {
         {
             name: "-2 segundos",
             code: "-2s"
+        },
+        {
+            name: "initial",
+            code: "initial"
+        },
+        {
+            name: "inherit",
+            code: "inherit"
         },
     ],
 }
