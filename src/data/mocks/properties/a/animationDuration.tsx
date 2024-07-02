f07c66190e85cc32f5c9d33beb20218c7ca782f7
Rapha Developer <contatosraphaelmoreira@gmail.com>
2024-07-02 18:22:37 -0300

    refactor: update function props to receive name & value. Import and use InheritTemplate and CopyAndPasteTemplate components. Remove unnecessary code

--- src/data/mocks/properties/a/animationDuration.tsx
+++ src/data/mocks/properties/a/animationDuration.tsx
@@ -1,17 +1,23 @@
+import { CopyAndPasteTemplate } from "../../../../components/example/CopyAndPasteTemplate";
+import { InheritTemplate } from "../../../../components/example/InheritTemplate";
+import { PropertyTemplateProps } from "../../../../types/PropertyTemplateProps";
 
-export default function animationDurationTemplate({ property }: {property: string}) {
+export default function animationDurationTemplate({ name, value }: PropertyTemplateProps['property']) {
+    const inheritValue = "2s"
     return (
-        <div className="flex flex-col gap-4" style={{animationDuration: "2s"}}>
+        <div className="flex flex-col gap-4" style={{animationDuration: inheritValue}}>
             <div className={`mx-auto h-12 w-12 rounded-md background-hero flex justify-center items-center animate-spin`}
-            style={{animationDuration: property}}>
+            style={{animationDuration: value}}>
                 <span className="font-outfit text-2xl font-semibold text-center text-body-200">01</span>
             </div>
-            <p  className="font-outfit text-secondary text-base text-center">Elemento pai tem animation-duration como&nbsp;
-                <span className="inline-block bg-yellow-200 px-2 rounded-md text-secondary font-outfit text-sm font-bold">2 segundos</span>
-            </p>
-            <p className="font-outfit text-secondary text-sm font-medium text-center bg-borderLine py-3 px-4 rounded-md w-fit mt-4 mx-auto">
-                animation-duration: {property}
-            </p>
+            <InheritTemplate 
+                property={{name: name, value: value}}
+                inheritValue={inheritValue}
+            />
+            <CopyAndPasteTemplate 
+                name={name}
+                value={value}
+            />
         </div>
     )
 }
