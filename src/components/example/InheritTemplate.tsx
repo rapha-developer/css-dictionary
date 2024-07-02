7da8f0af0eac88301ce36dd58fd86422913186da
Rapha Developer <contatosraphaelmoreira@gmail.com>
2024-07-02 18:25:45 -0300

    refactor: add margin-top to paragraph element

--- src/components/example/InheritTemplate.tsx
+++ src/components/example/InheritTemplate.tsx
@@ -2,7 +2,7 @@ import { InheritTemplateProps } from "../../types/PropertyTemplateProps";
 
 export const InheritTemplate = ({ property, inheritValue }: InheritTemplateProps) => {
     return (
-        <p  className="font-outfit text-secondary text-base text-center">Elemento pai tem {property.name} como&nbsp;
+        <p  className="font-outfit text-secondary text-base text-center mt-4">Elemento pai tem {property.name} como&nbsp;
             <span className="inline-block bg-yellow-200 px-2 rounded-md text-secondary font-outfit text-sm font-bold">{inheritValue}</span>
         </p>
     )
