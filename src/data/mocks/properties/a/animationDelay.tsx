146463cf51037c159f701eff420ae0fa2499ede1
Rapha Developer <contatosraphaelmoreira@gmail.com>
2024-07-02 12:15:38 -0300

    refactor: import and use CopyAndPasteTemplate & InheritTemplate. Create a inherit value. And show the css property with all the information

--- src/data/mocks/properties/a/animationDelay.tsx
+++ src/data/mocks/properties/a/animationDelay.tsx
@@ -1,9 +1,23 @@
+import { CopyAndPasteTemplate } from "../../../../components/example/CopyAndPasteTemplate";
+import { InheritTemplate } from "../../../../components/example/InheritTemplate";
+import { PropertyTemplateProps } from "../../../../types/PropertyTemplateProps";
 
-export default function AnimationDelayTemplate({ property }: { property: string }) {
+export default function AnimationDelayTemplate({ name, value }: PropertyTemplateProps['property']) {
+    const inheritValue = "1.5s"
     return (
-        <div className={`h-12 w-12 rounded-md background-hero flex justify-center items-center animate-bounce`}
-            style={{animationDelay: property}}>
-            <span className="font-outfit text-2xl font-semibold text-center text-body-200">01</span>
+        <div className="flex flex-col gap-4" style={{animationDelay: inheritValue}}>
+            <div className={`mx-auto h-12 w-12 rounded-md background-hero flex justify-center items-center animate-bounce`}
+                style={{animationDelay: value}}>
+                <span className="font-outfit text-2xl font-semibold text-center text-body-200">01</span>
+            </div>
+            <InheritTemplate 
+                property={{name: name, value: value}}
+                inheritValue={inheritValue}
+            />
+            <CopyAndPasteTemplate 
+                name={name}
+                value={value}
+            />
         </div>
     )
 } 
