bf85f9d231c162bf5c5f1362845fac3fb4baf5ca
Rapha Developer <contatosraphaelmoreira@gmail.com>
2024-07-02 18:13:15 -0300

    refactor: update function props to receive name & value. Import and use inheritTemplate and CopyAndPasteTemplate components. Remove unnecessary code

--- src/data/mocks/properties/a/all.tsx
+++ src/data/mocks/properties/a/all.tsx
@@ -1,17 +1,21 @@
+import { CopyAndPasteTemplate } from "../../../../components/example/CopyAndPasteTemplate"
+import { InheritTemplate } from "../../../../components/example/InheritTemplate"
 
 
-export default function AllTemplate({ property }: {property: "initial" | "inherit" | "unset" | "revert" | "revert-layer"}) {
-    const styles = {all: property}
+export default function AllTemplate({ name, value }: {name: string, value: "initial" | "inherit" | "unset" | "revert" | "revert-layer"}) {
+    const styles = {all: value}
     return (
         <div className="flex flex-col gap-3 font-mono text-base py-2">
             <p className="font-outfit text-lg text-secondary bg-yellow-200 text-center"
             style={styles}>Exemplo aqui...</p>
-            <p className="font-outfit text-secondary text-sm font-medium text-center bg-borderLine py-3 px-4 rounded-md w-fit mx-auto">
-                all: {property}
-            </p>
-            <p  className="font-outfit text-secondary text-base">Elemento pai tem font-family como&nbsp;
-                <span className="inline-block bg-yellow-200 px-2 rounded-md text-secondary font-outfit text-sm font-bold">mono</span>
-            </p>
+            <CopyAndPasteTemplate 
+                name={name}
+                value={value}
+            />
+            <InheritTemplate 
+                inheritValue={'mono'}
+                property={{name: name, value: value}}
+            />
             <p className="flex justify-center">
                 <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/all#formal_definition"
                 target="_blank"
