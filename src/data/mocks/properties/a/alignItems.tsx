1e0b2eb4a87c497c098fc857dea1c63e0819af2d
Rapha Developer <contatosraphaelmoreira@gmail.com>
2024-07-02 18:07:26 -0300

    refactor: update function props to receive name & value. Import and use inheritTemplate and CopyAndPasteTemplate components. Remove unnecessary code

--- src/data/mocks/properties/a/alignItems.tsx
+++ src/data/mocks/properties/a/alignItems.tsx
@@ -1,6 +1,8 @@
+import { CopyAndPasteTemplate } from "../../../../components/example/CopyAndPasteTemplate";
+import { InheritTemplate } from "../../../../components/example/InheritTemplate";
+import { PropertyTemplateProps } from "../../../../types/PropertyTemplateProps";
 
-
-export default function AlignItemsTemplate({ property }: {property: string}) {
+export default function AlignItemsTemplate({ name, value }: PropertyTemplateProps['property']) {
     return (
         <div className="w-full h-auto">
             <div className="bg-dark mx-auto w-3/4 h-64 py-4 px-2 rounded-md overflow-hidden relative">
@@ -8,9 +10,9 @@ export default function AlignItemsTemplate({ property }: {property: string}) {
                     alt="Dark grid" 
                     className="absolute top-0 left-0 object-cover z-10 w-full h-full"
                 />
-                <div className="absolute top-0 left-0 z-20 w-full h-full py-3">
+                <div className="absolute top-0 left-0 z-20 w-full h-full py-3 items-center">
                     <div className="w-full py-2 px-2 grid grid-cols-3 gap-4 h-60"
-                        style={{alignItems: property}}>
+                        style={{alignItems: value}}>
                         {[1,2,3,4].map((item) => (
                             <div className="w-12 h-12 background-hero rounded-md" key={`item-${item}`}>
                                 <span className="font-outfit text-2xl font-bold py-1 px-3 text-body-200 flex items-center">0{item}</span>
@@ -19,9 +21,14 @@ export default function AlignItemsTemplate({ property }: {property: string}) {
                     </div>
                 </div>
             </div>
-            <p className="font-outfit text-secondary text-sm font-medium text-center bg-borderLine py-3 px-4 rounded-md w-fit mt-4 mx-auto">
-                align-items: {property}
-            </p>
+            <InheritTemplate 
+                inheritValue={'center'}
+                property={{name: name, value: value}}
+            />
+            <CopyAndPasteTemplate 
+                name={name}
+                value={value}
+            />
         </div>
     )
 }
