ea1c77e3f1be6ec24c829483ef7962d97e929b1a
Rapha Developer <contatosraphaelmoreira@gmail.com>
2024-07-05 11:37:52 -0300

    refactor: change bounce animation to ping animation

--- src/data/mocks/properties/a/animationDelay.tsx
+++ src/data/mocks/properties/a/animationDelay.tsx
@@ -6,7 +6,7 @@ export default function AnimationDelayTemplate({ name, value }: PropertyTemplate
     const inheritValue = "1.5s"
     return (
         <div className="flex flex-col gap-4" style={{animationDelay: inheritValue}}>
-            <div className={`mx-auto h-12 w-12 rounded-md background-hero flex justify-center items-center animate-bounce`}
+            <div className={`mx-auto h-12 w-12 rounded-md background-hero flex justify-center items-center animate-ping`}
                 style={{animationDelay: value}}>
                 <span className="font-outfit text-2xl font-semibold text-center text-body-200">01</span>
             </div>
