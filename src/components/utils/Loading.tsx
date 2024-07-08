7fbd9818bacca92701be79334e8c5d76c7f90582
Rapha Developer <contatosraphaelmoreira@gmail.com>
2024-07-08 18:43:38 -0300

    feat: export loading component. Responsible for loading components before lazy import

---
+++ src/components/utils/Loading.tsx
@@ -0,0 +1,10 @@
+
+
+export const LoadingUtil = () => {
+    return (
+        <img src="/loading.gif" 
+            alt="Loading gif" 
+            className="w-24 h-24 mx-auto"
+        />
+    )
+}
