19e5253fe27f0f8901ede376a41466d16782fa50
Rapha Developer <contatosraphaelmoreira@gmail.com>
2024-07-09 16:26:36 -0300

    refactor: change backgroundPosition to backgroundPositionX property. Change height property. add border radius to div

--- src/data/mocks/properties/b/backgroundPositionX.tsx
+++ src/data/mocks/properties/b/backgroundPositionX.tsx
@@ -6,10 +6,10 @@ export default function BackgroundPositionXTemplate({ name, value }: PropertyTem
     const inheritValue = `45%`
     return (
         <div className="w-full h-full flex flex-col gap-4 py-6 px-2 md:px-4 justify-center">
-            <div className="w-full h-96 bg-borderLine p-2 md:p-6" style={{backgroundPosition: inheritValue}}>
+            <div className="w-full h-60 rounded-md bg-borderLine p-2 md:p-6" style={{backgroundPositionX: inheritValue}}>
                 <div className="w-full h-full"
             style={{
-                backgroundPosition: value,
+                backgroundPositionX: value,
                 backgroundRepeat: 'no-repeat',
                 backgroundImage: 'url("https://interactive-examples.mdn.mozilla.net/media/examples/star.png")'}}></div>
             </div>
