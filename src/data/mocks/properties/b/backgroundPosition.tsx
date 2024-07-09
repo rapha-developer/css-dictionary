1f86456ac582e382f748ba1d9fe8ac133c5a3f9d
Rapha Developer <contatosraphaelmoreira@gmail.com>
2024-07-09 13:17:17 -0300

    refactor: remove unnecessary styles

--- src/data/mocks/properties/b/backgroundPosition.tsx
+++ src/data/mocks/properties/b/backgroundPosition.tsx
@@ -5,8 +5,7 @@ import { PropertyTemplateProps } from "../../../../types/PropertyTemplateProps";
 export default function BackgroundPositionTemplate({ name, value }: PropertyTemplateProps['property']) {
     const inheritValue = `left center`
     return (
-        <div className="w-full h-full flex flex-col gap-4 py-6 px-2 md:px-4 justify-center" 
-            style={{backgroundPosition: inheritValue}}>
+        <div className="w-full h-full flex flex-col gap-4 py-6 px-2 md:px-4 justify-center">
             <div className="w-full h-96 bg-borderLine p-2 md:p-6" style={{backgroundPosition: inheritValue}}>
                 <div className="w-full h-full"
             style={{
