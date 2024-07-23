de2c0226f72b08a495a8b84e0eb3f189c4346931
Rapha Developer <contatosraphaelmoreira@gmail.com>
2024-07-23 11:40:45 -0300

    feat: export border top data constant. Export border top template for border top examples

---
+++ src/data/mocks/properties/b/borderTop.tsx
@@ -0,0 +1,23 @@
+import { CopyAndPasteTemplate } from "../../../../components/example/CopyAndPasteTemplate";
+import { InheritTemplate } from "../../../../components/example/InheritTemplate";
+import { PropertyTemplateProps } from "../../../../types/PropertyTemplateProps";
+
+export default function BorderTopTemplate({ name, value }: PropertyTemplateProps['property']) {
+    const inheritValue = `2px dotted crimson`
+    return (
+        <div className="flex flex-col gap-4 py-4" style={{borderTop: inheritValue}}>
+            <div className="w-3/4 h-20 rounded-md mx-auto flex items-center justify-center"
+            style={{borderTop: value}}>
+                <p className="font-outfit text-sm text-secondary font-normal text-center">Muda a borda direita</p>
+            </div>
+            <InheritTemplate 
+                inheritValue={inheritValue}
+                property={{name: name, value: value}}
+            />
+            <CopyAndPasteTemplate 
+                name={name}
+                value={value}
+            />
+        </div>
+    )
+}
