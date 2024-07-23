e5fc91997ed871eba042d1530ed669bdcdd9a9b5
Rapha Developer <contatosraphaelmoreira@gmail.com>
2024-07-23 11:03:15 -0300

    refactor: show property name replacing whitespace with dashes

--- src/components/example/CopyAndPasteTemplate.tsx
+++ src/components/example/CopyAndPasteTemplate.tsx
@@ -3,7 +3,7 @@ import { CopyAndPasteTemplateProps } from "../../types/PropertyTemplateProps"
 export const CopyAndPasteTemplate = ({ name, value }: CopyAndPasteTemplateProps) => {
     return (
         <p className="font-outfit text-secondary text-sm font-medium text-center bg-borderLine py-3 px-4 rounded-md w-fit mt-4 mx-auto">
-            {name}: {value}
+            {name.replace(/\s/g, '-')}: {value}
         </p>
     )
 }
