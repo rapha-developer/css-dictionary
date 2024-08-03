83cbe8581f842b4434572ded257aa6fdc1479d4f
Rapha Developer <contatosraphaelmoreira@gmail.com>
2024-08-03 17:38:02 -0300

    mocks: export properties... with b course info. Responsible for all course info, including resume, body (title) and curriculum from that specific course

---
+++ src/data/mocks/courses/info/propertiesStartWithBInfo.ts
@@ -0,0 +1,10 @@
+import { CourseInfoProps } from "../../../../types/CourseProps";
+
+export const propertiesStartWithBCourseInfo: CourseInfoProps = {
+    resume: "Neste curso, você aprenderá 41 propriedades css que começam com a letra B atráves de vários exemplos. Aumente seus conhecimentos na área de desenvolvimento web ",
+    body: {
+        first: "Dentre as propriedades que você aprenderá estão: .",
+        second: "Cada propriedade css contém um nome, uma nítida e detalhada definição do que faz, como também os valores de entrada formalmente aceitos pela linguagem de estilização CSS. Além de númerosos exemplos que buscam esclarecer o que cada valor realmente executa dentro de uma aplicação web."
+    },
+    curriculumDesc: "Esse curso foi construído com o intuito de entender minunciosamente cada propriedade css que começa com letra B. Todas as 41 propriedades, quando visualizadas com atenção, podem auxiliar desenvolvedores na resolução de desafios com a linguagem css.",
+}
