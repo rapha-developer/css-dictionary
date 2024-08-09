40c51094b53074a7af755a62a1366dd316517091
Rapha Developer <contatosraphaelmoreira@gmail.com>
2024-08-09 18:41:39 -0300

    feat: export column gap data constant. Export column gap template for column gap examples

---
+++ src/data/mocks/properties/c/data/columnGap.ts
@@ -0,0 +1,40 @@
+import { PropertyProps } from "../../../../../types/PropertyProps";
+import { genericPropertyExamplesData, genericPropertyValuesData } from "../../../GenericPropertyAtributes";
+
+export const columnGapData: PropertyProps = {
+    name: "column gap",
+    definition: "A propriedade column-gap especifica o espaço entre as colunas no layout de grade, flexbox ou multicolunas. Se existir um column-rule, ele aparecerá entre as colunas.",
+    category: "css property",
+    values: [
+        {
+            name: "length",
+            value: "Um comprimento especificado que definirá o espaço entre as colunas"
+        },
+        {
+            name: "normal",
+            value: "Valor padrão. Especifica um espaço normal entre as colunas. O W3C sugere um valor de 1em"
+        },
+        genericPropertyValuesData.initial,
+        genericPropertyValuesData.inherit
+    ],
+    examples: [
+        {
+            name: "10px",
+            code: "10px"
+        },
+        {
+            name: "30px",
+            code: "30px"
+        },
+        {
+            name: "70px",
+            code: "70px"
+        },
+        {
+            name: "normal",
+            code: "normal"
+        },
+        genericPropertyExamplesData.initial,
+        genericPropertyExamplesData.inherit
+    ]
+}
