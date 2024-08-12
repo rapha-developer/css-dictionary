89c6eb41434ab339a5e3c758edeba535d81895f2
Rapha Developer <contatosraphaelmoreira@gmail.com>
2024-08-12 11:32:39 -0300

    text: fix wrong accent in a word

--- src/data/mocks/courses/info/propertiesStartWithAInfo.ts
+++ src/data/mocks/courses/info/propertiesStartWithAInfo.ts
@@ -1,7 +1,7 @@
 import { CourseInfoProps } from "../../../../types/CourseProps";
 
 export const propertiesStartWithACourseInfo: CourseInfoProps = {
-    resume: "Neste curso, você aprenderá 14 propriedades css que começam com a letra A atráves de vários exemplos. Aumente seus conhecimentos na área de desenvolvimento web ",
+    resume: "Neste curso, você aprenderá 14 propriedades css que começam com a letra A através de vários exemplos. Aumente seus conhecimentos na área de desenvolvimento web ",
     body: {
         first: "Dentre as propriedades que você aprenderá estão: accent-color, align-content, align-items, all, animation, animation-delay, animation-direction, animation-fill-mode, animation-iteration-count, animation-name, animation-play-state, animation-timing-function e não menos importante: aspect-ratio.",
         second: "Cada propriedade css contém um nome, uma nítida e detalhada definição do que faz, como também os valores de entrada formalmente aceitos pela linguagem de estilização CSS. Além de númerosos exemplos que buscam esclarecer o que cada valor realmente executa dentro de uma aplicação web."
