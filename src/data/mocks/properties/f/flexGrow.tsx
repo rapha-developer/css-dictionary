a657e21be5dc76518fde8ffdd63623f187146073
Rapha Developer <contatosraphaelmoreira@gmail.com>
2024-08-22 10:39:43 -0300

    indent: remove unnecessary space

--- src/data/mocks/properties/f/flexGrow.tsx
+++ src/data/mocks/properties/f/flexGrow.tsx
@@ -8,16 +8,16 @@ export default function FlexGrowTemplate({ name, value }: PropertyTemplateProps[
         <div className="flex flex-col gap-4 px-4 py-6 w-full relative">
             <div className="relative w-full h-64 md:h-44 py-5 px-2">
                 <div className="flex-container flex flex-wrap relative z-20" style={{flexGrow: inheritValue}}>
-                    <div className=" h-10 bg-icon-widget-red p-2" style={{flexGrow: '1'}}>
+                    <div className="h-10 bg-icon-widget-red p-2" style={{flexGrow: '1'}}>
                         red div
                     </div>
-                    <div className=" h-10 bg-icon-widget-emerald p-2" style={{flexGrow: value}}>
+                    <div className="h-10 bg-icon-widget-emerald p-2" style={{flexGrow: value}}>
                         emerald div
                     </div>
-                    <div className=" h-10 bg-icon-widget-gold p-2" style={{flexGrow: '1'}}>
+                    <div className="h-10 bg-icon-widget-gold p-2" style={{flexGrow: '1'}}>
                         gold div
                     </div>
-                    <div className=" h-10 bg-icon-widget-pink p-2" style={{flexGrow: '1'}}>
+                    <div className="h-10 bg-icon-widget-pink p-2" style={{flexGrow: '1'}}>
                         pink div
                     </div>
                 </div>
