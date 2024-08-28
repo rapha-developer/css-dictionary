e98114a8e6ae9024913b8e601764a6af1ab441bd
Rapha Developer <contatosraphaelmoreira@gmail.com>
2024-08-28 10:27:04 -0300

    feat: export (properties start with F course info) data constant. Responsible to generate information about this course

---
+++ src/data/mocks/courses/info/propertiesStartWithFInfo.ts
@@ -0,0 +1,10 @@
+import { CourseInfoProps } from "../../../../types/CourseProps";
+
+export const propertiesStartWithFCourseInfo: CourseInfoProps = {
+    resume: "Neste curso, você aprenderá 18 propriedades css que começa com a letra F através de vários exemplos. Aumente seus conhecimentos na área de desenvolvimento web dentro da linguagem css",
+    body: {
+        first: "Dentre as propriedades que você aprenderá estão: filter, flex, flex-basis, flex-direction, flex-flow, flex-grow, flex-shrink, float, font-family, font-size, font-size, font-weight & etc.",
+        second: "Cada propriedade css contém um nome, uma nítida e detalhada definição do que faz, como também os valores de entrada formalmente aceitos pela linguagem de estilização CSS. Além de númerosos exemplos que buscam esclarecer o que cada valor realmente executa dentro de uma aplicação web."
+    },
+    curriculumDesc: "Esse curso foi construído com o intuito de entender minunciosamente cada propriedade css que começa com letra F. As propriedades, quando visualizadas com atenção, podem auxiliar desenvolvedores na resolução de desafios com a linguagem css.",
+}
