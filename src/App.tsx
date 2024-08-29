5a9b5a157927b27c285414e970b8777006528296
Rapha Developer <contatosraphaelmoreira@gmail.com>
2024-08-29 10:35:45 -0300

    import react-router-dom and make the necessary routes for the application to work correctly with the imported component pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,29 @@
-// import { useState } from 'react'
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import './fonts.css'
 import './App.css'
+import { HomePage } from './components/pages/HomePage';
+import { CoursePage } from './components/pages/CoursePage';
+import { ModulePage } from './components/pages/ModulePage';
+import { CourseListPage } from './components/pages/CourseListPage';
+import { GlossaryPage } from './components/pages/GlossaryPage';
+import { TermPage } from './components/pages/TermPage';
+import { AboutPage } from './components/pages/AboutPage';
 
 function App() {
-  // const [count, setCount] = useState(0)
-
+  
   return (
-    <>
-      <h1 className="font-sans text-2xl text-purple-700">
-        Te amo
-      </h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/glossary" element={<GlossaryPage />} />
+        <Route path="/glossary/:termID" element={<TermPage />} />
+        <Route path="/courses" element={<CourseListPage />} />
+        <Route path="/courses/:courseID" element={<CoursePage />} />
+        <Route path="/courses/:courseID/modules/:moduleID" element={<ModulePage />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+  </BrowserRouter>
   )
 }
 
