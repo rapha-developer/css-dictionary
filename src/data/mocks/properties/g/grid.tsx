726ad2619539f79d2a9321152494e6a5c80bcfa3
Rapha Developer <contatosraphaelmoreira@gmail.com>
2024-09-02 10:26:20 -0300

    refactor: change text-md to text-xl

--- src/data/mocks/properties/g/grid.tsx
+++ src/data/mocks/properties/g/grid.tsx
@@ -7,12 +7,12 @@ export default function GridTemplate({ name, value }: PropertyTemplateProps['pro
     return (
         <div className="flex flex-col gap-4 px-4 py-6 w-full" style={{grid: inheritValue}}>
             <div className="w-full h-full grid gap-3 py-3 px-2 bg-pink-300" style={{grid: value}}>
-                <div className="w-full h-full p-5 text-center text-md bg-pink-200">1</div>
-                <div className="w-full h-full p-5 text-center text-md bg-pink-200">2</div>
-                <div className="w-full h-full p-5 text-center text-md bg-pink-200">3</div>
-                <div className="w-full h-full p-5 text-center text-md bg-pink-200">4</div>
-                <div className="w-full h-full p-5 text-center text-md bg-pink-200">5</div>
-                <div className="w-full h-full p-5 text-center text-md bg-pink-200">6</div>
+                <div className="w-full h-full p-5 text-center text-xl bg-pink-200">1</div>
+                <div className="w-full h-full p-5 text-center text-xl bg-pink-200">2</div>
+                <div className="w-full h-full p-5 text-center text-xl bg-pink-200">3</div>
+                <div className="w-full h-full p-5 text-center text-xl bg-pink-200">4</div>
+                <div className="w-full h-full p-5 text-center text-xl bg-pink-200">5</div>
+                <div className="w-full h-full p-5 text-center text-xl bg-pink-200">6</div>
             </div>
             <InheritTemplate 
                 inheritValue={inheritValue}
--- src/data/mocks/properties/g/gridAutoFlow.tsx
+++ src/data/mocks/properties/g/gridAutoFlow.tsx
@@ -5,12 +5,12 @@ export default function GridAutoFlowTemplate({ name, value }: PropertyTemplatePr
     return (
         <div className="flex flex-col gap-4 px-4 py-6 w-full" >
             <div className="w-full h-full grid gap-3 py-3 px-2 bg-pink-300 rounded-lg" style={{gridTemplateColumns: 'auto auto auto', gridTemplateRows: 'auto auto', gridAutoFlow: value}}>
-                <div className="w-full h-full p-5 text-center text-md bg-pink-200 rounded-lg">1</div>
-                <div className="w-full h-full p-5 text-center text-md bg-pink-200 rounded-lg">2</div>
-                <div className="w-full h-full p-5 text-center text-md bg-pink-200 rounded-lg" style={{gridColumn: 'auto / span 2'}}>3</div>
-                <div className="w-full h-full p-5 text-center text-md bg-pink-200 rounded-lg">4</div>
-                <div className="w-full h-full p-5 text-center text-md bg-pink-200 rounded-lg">5</div>
-                <div className="w-full h-full p-5 text-center text-md bg-pink-200 rounded-lg">6</div>
+                <div className="w-full h-full p-5 text-center text-xl bg-pink-200 rounded-lg">1</div>
+                <div className="w-full h-full p-5 text-center text-xl bg-pink-200 rounded-lg">2</div>
+                <div className="w-full h-full p-5 text-center text-xl bg-pink-200 rounded-lg" style={{gridColumn: 'auto / span 2'}}>3</div>
+                <div className="w-full h-full p-5 text-center text-xl bg-pink-200 rounded-lg">4</div>
+                <div className="w-full h-full p-5 text-center text-xl bg-pink-200 rounded-lg">5</div>
+                <div className="w-full h-full p-5 text-center text-xl bg-pink-200 rounded-lg">6</div>
             </div>
             <CopyAndPasteTemplate 
                 name={name}
