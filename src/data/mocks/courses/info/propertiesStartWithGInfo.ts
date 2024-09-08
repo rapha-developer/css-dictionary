f87dc3ca9f39399a360b6a99705d1a77777a9fb6
Rapha Developer <contatosraphaelmoreira@gmail.com>
2024-09-08 08:22:13 -0300

    feat: export (properties start with G course info) data constant. Responsible to generate information about this course

---
+++ src/data/mocks/courses/info/propertiesStartWithGInfo.ts
@@ -0,0 +1,10 @@
+import { CourseInfoProps } from "../../../../types/CourseProps";
+
+export const propertiesStartWithGCourseInfo: CourseInfoProps = {
+    resume: "Neste curso, você aprenderá 16 propriedades css que começa com a letra G através de vários exemplos. Aumente seus conhecimentos na área de desenvolvimento web dentro da linguagem css",
+    body: {
+        first: "Dentre as propriedades que você aprenderá estão: gap, grid, grid-area, grid-auto-columns, grid-auto-flow, grid-auto-rows, grid-column, grid-row, grid-template, grid-template-areas, grid-template-columns and grid-template-rows.",
+        second: "Cada propriedade css contém um nome, uma nítida e detalhada definição do que faz, como também os valores de entrada formalmente aceitos pela linguagem de estilização CSS. Além de númerosos exemplos que buscam esclarecer o que cada valor realmente executa dentro de uma aplicação web."
+    },
+    curriculumDesc: "Esse curso foi construído com o intuito de entender minunciosamente cada propriedade css que começa com letra G. As propriedades, quando visualizadas com atenção, podem auxiliar desenvolvedores na resolução de desafios com a linguagem css.",
+}
