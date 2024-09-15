76a83ece05ada69ba24cfc4e6ca11cb61eabd1c9
Rapha Developer <contatosraphaelmoreira@gmail.com>
2024-09-15 09:00:02 -0300

    feat: export (properties start with I course info) data constant. Responsible to generate information about this course

---
+++ src/data/mocks/courses/info/propertiesStartWithIInfo.ts
@@ -0,0 +1,10 @@
+import { CourseInfoProps } from "../../../../types/CourseProps";
+
+export const propertiesStartWithICourseInfo: CourseInfoProps = {
+    resume: "Neste curso, você aprenderá 10 propriedades css que começa com a letra I através de vários exemplos. Aumente seus conhecimentos na área de desenvolvimento web dentro da linguagem css",
+    body: {
+        first: "Dentre as propriedades que você aprenderá estão: image-rendering, inline-size, inset, inset-block, inset-block-end, inset-block-start, inset-inline, inset-inline-end, inset-inline-start & isolation.",
+        second: "Cada propriedade css contém um nome, uma nítida e detalhada definição do que faz, como também os valores de entrada formalmente aceitos pela linguagem de estilização CSS. Além de númerosos exemplos que buscam esclarecer o que cada valor realmente executa dentro de uma aplicação web."
+    },
+    curriculumDesc: "Esse curso foi construído com o intuito de entender minunciosamente cada propriedade css que começa com letra I. As propriedades, quando visualizadas com atenção, podem auxiliar desenvolvedores na resolução de desafios com a linguagem css.",
+}
