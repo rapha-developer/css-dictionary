85001e78ca2c14bca363676d49808a465a43737d
Rapha Developer <contatosraphaelmoreira@gmail.com>
2024-09-23 10:32:40 -0300

    feat: export list-style-type data constant (with all information about property). Export listy style type template for list style type examples

---
+++ src/data/mocks/properties/l/data/listStyleType.ts
@@ -0,0 +1,188 @@
+import { PropertyProps } from "../../../../../types/PropertyProps";
+import { genericPropertyExamplesData, genericPropertyValuesData } from "../../../GenericPropertyAtributes";
+
+export const listStyleTypeData: PropertyProps = {
+    name: "list style type",
+    definition: "O list-style-type especifica o tipo de marcador de item de lista em uma lista.",
+    category: "css property",
+    values: [
+        {
+            name: "inside",
+            value: "Valor padrão. O marcador é um círculo preenchido"
+        },
+        {
+            name: "armenian",
+            value: "O marcador é numeração armênia tradicional"
+        },
+        {
+            name: "circle",
+            value: "O marcador é um círculo"
+        },
+        {
+            name: "cjk-ideographic",
+            value: "O marcador é números ideográficos simples decimal O marcador é um número"
+        },
+        {
+            name: "decimal-leading-zero",
+            value: "O marcador é um número com zeros à esquerda (01, 02, 03, etc.)"
+        },
+        {
+            name: "georgian",
+            value: "O marcador é numeração georgiana tradicional"
+        },
+        {
+            name: "hebrew",
+            value: "O marcador é numeração hebraica tradicional"
+        },
+        {
+            name: "hiragana",
+            value: "O marcador é numeração hiragana tradicional"
+        },
+        {
+            name: "hiragana-iroha",
+            value: "O marcador é numeração hiragana tradicional iroha"
+        },
+        {
+            name: "katakana",
+            value: "O marcador é numeração katakana tradicional"
+        },
+        {
+            name: "katakana-iroha",
+            value: "O marcador é numeração katakana tradicional iroha"
+        },
+        {
+            name: "lower-alpha",
+            value: "O marcador é lower-alpha (a, b, c, d, e, etc.)"
+        },
+        {
+            name: "lower-greek",
+            value: "O marcador é lower-greek"
+        },
+        {
+            name: "lower-latin",
+            value: "O marcador é lower-latin (a, b, c, d, e, etc.)"
+        },
+        {
+            name: "lower-roman",
+            value: "O marcador é baixo-romano (i, ii, iii, iv, v, etc.)"
+        },
+        {
+            name: "none",
+            value: "Nenhum marcador é mostrado"
+        },
+        {
+            name: "square",
+            value: "O marcador é um quadrado"
+        },
+        {
+            name: "upper-alpha",
+            value: "O marcador é alto-alfa (A, B, C, D, E, etc.)"
+        },
+        {
+            name: "upper-greek",
+            value: "O marcador é alto-grego"
+        },
+        {
+            name: "upper-latin",
+            value: "O marcador é alto-latino (A, B, C, D, E, etc.)"
+        },
+        {
+            name: "upper-roman",
+            value: "O marcador é alto-romano (I, II, III, IV, V, etc.)"
+        },
+        genericPropertyValuesData.initial,
+        genericPropertyValuesData.inherit
+    ],
+    examples: [
+        {
+            name: "disc",
+            code: "disc"
+        },
+        {
+            name: "circle",
+            code: "circle"
+        },
+        {
+            name: "square",
+            code: "square"
+        },
+        {
+            name: "decimal",
+            code: "decimal"
+        },
+        {
+            name: "decimal-leading-zero",
+            code: "decimal-leading-zero"
+        },
+        {
+            name: "lower-alpha",
+            code: "lower-alpha"
+        },
+        {
+            name: "lower-greek",
+            code: "lower-greek"
+        },
+        {
+            name: "lower-latin",
+            code: "lower-latin"
+        },
+        {
+            name: "lower-roman",
+            code: "lower-roman"
+        },
+        {
+            name: "upper-alpha",
+            code: "upper-alpha"
+        },
+        {
+            name: "upper-greek",
+            code: "upper-greek"
+        },
+        {
+            name: "upper-latin",
+            code: "upper-latin"
+        },
+        {
+            name: "upper-roman",
+            code: "upper-roman"
+        },
+        {
+            name: "armenian",
+            code: "armenian"
+        },
+        {
+            name: "cjk-ideographic",
+            code: "cjk-ideographic"
+        },
+        {
+            name: "georgian",
+            code: "georgian"
+        },
+        {
+            name: "hebrew",
+            code: "hebrew"
+        },
+        {
+            name: "hiragana",
+            code: "hiragana"
+        },
+        {
+            name: "hiragana-iroha",
+            code: "hiragana-iroha"
+        },
+        {
+            name: "none",
+            code: "none"
+        },
+        {
+            name: "katakana",
+            code: "katakana"
+        },
+        {
+            name: "katakana-iroha",
+            code: "katakana-iroha"
+        },
+        genericPropertyExamplesData.initial,
+        genericPropertyExamplesData.inherit
+    ]
+}
