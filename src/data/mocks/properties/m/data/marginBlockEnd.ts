48f336ad946071494b0621c545e15b27b7f97bb3
Rapha Developer <contatosraphaelmoreira@gmail.com>
2024-09-26 09:14:03 -0300

    feat: export margin-block-end data constant (with all information about property). Export margin block end template for margin block end examples

---
+++ src/data/mocks/properties/m/data/marginBlockEnd.ts
@@ -0,0 +1,60 @@
+import { PropertyProps } from "../../../../../types/PropertyProps";
+import { genericPropertyExamplesData, genericPropertyValuesData } from "../../../GenericPropertyAtributes";
+
+export const marginBlockEndData: PropertyProps = {
+    name: "margin block end",
+    definition: "A propriedade margin-block-end especifica a margem no final na direção do bloco. As propriedades CSS margin-block e margin-inline são muito semelhantes às propriedades CSS margin-top, margin-bottom, margin-left e margin-right, mas as propriedades margin-block e margin-inline dependem das direções block e inline.",
+    category: "css property",
+    values: [
+        {
+            name: "auto",
+            value: "Padrão. A margem padrão do elemento."
+        },
+        {
+            name: "length",
+            value: "Especifica a distância em px, pt, cm, etc. Valores negativos são permitidos. Leia sobre unidades de comprimento no glossário."
+        },
+        {
+            name: "%",
+            value: "Especifica a distância em porcentagem relativa ao tamanho do elemento pai na direção inline."
+        },
+        genericPropertyValuesData.initial,
+        genericPropertyValuesData.inherit
+    ],
+    examples: [
+        {
+            name: "0",
+            code: "0"
+        },
+        {
+            name: "10px",
+            code: "10px"
+        },
+        {
+            name: "25px",
+            code: "25px"
+        },
+        {
+            name: "50px",
+            code: "50px"
+        },
+        {
+            name: "100px",
+            code: "100px"
+        },
+        {
+            name: "auto",
+            code: "auto"
+        },
+        {
+            name: "5%",
+            code: "5%"
+        },
+        {
+            name: "10%",
+            code: "10%"
+        },
+        genericPropertyExamplesData.initial,
+        genericPropertyExamplesData.inherit
+    ]
+}
