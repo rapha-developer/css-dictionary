2a06dcc1bd09cc935b140b1b1b7c9e17fd26109e
Rapha Developer <contatosraphaelmoreira@gmail.com>
2024-09-27 10:46:40 -0300

    feat: export margin-inline data constant (with all information about property). Export margin inline template for margin inline examples

---
+++ src/data/mocks/properties/m/data/marginInline.ts
@@ -0,0 +1,56 @@
+import { PropertyProps } from "../../../../../types/PropertyProps";
+import { genericPropertyExamplesData, genericPropertyValuesData } from "../../../GenericPropertyAtributes";
+
+export const marginInlineData: PropertyProps = {
+    name: "margin inline",
+    definition: "A propriedade margin-inline especifica a margem no início e no fim na direção inline e é uma propriedade abreviada para as seguintes propriedades: margin-inline-start & margin-inline-end. As propriedades CSS margin-inline e margin-block são muito semelhantes às propriedades CSS margin-top, margin-bottom, margin-left e margin-right, mas as propriedades margin-inline e margin-block dependem das direções block e inline.",
+    category: "css property",
+    values: [
+        {
+            name: "auto",
+            value: "Padrão. Valor margin-inline padrão do elemento."
+        },
+        {
+            name: "length",
+            value: "Especifica margin-inline em px, pt, cm, etc. Valores negativos são permitidos. Leia sobre unidades de comprimento."
+        },
+        {
+            name: "%",
+            value: "Especifica a margem embutida em porcentagem relativa ao tamanho do elemento pai na direção embutida."
+        },
+        genericPropertyValuesData.initial,
+        genericPropertyValuesData.inherit
+    ],
+    examples: [
+        {
+            name: "0",
+            code: "0"
+        },
+        {
+            name: "10px",
+            code: "10px"
+        },
+        {
+            name: "-25px",
+            code: "-25px"
+        },
+        {
+            name: "50px",
+            code: "50px"
+        },
+        {
+            name: "auto",
+            code: "auto"
+        },
+        {
+            name: "5%",
+            code: "5%"
+        },
+        {
+            name: "10px 50px",
+            code: "10px 50px"
+        },
+        genericPropertyExamplesData.initial,
+        genericPropertyExamplesData.inherit
+    ]
+}
---
+++ src/data/mocks/properties/m/marginInline.tsx
@@ -0,0 +1,25 @@
+import { CopyAndPasteTemplate } from "../../../../components/example/CopyAndPasteTemplate"
+import { InheritTemplate } from "../../../../components/example/InheritTemplate"
+import { PropertyTemplateProps } from "../../../../types/PropertyTemplateProps"
+
+export default function MarginInlineTemplate({ name, value }: PropertyTemplateProps['property']) {
+    const inheritValue = `2px`
+    return (
+        <div className="flex flex-col gap-4 px-4 py-6 w-full">
+            <div className="w-full h-full rounded-md flex" style={{marginInline: inheritValue}}>
+                <div className="w-full h-10 bg-pink-200 rounded-md font-outfit text-xl text-secondary font-semibold text-center">1</div>
+                <div className="w-full h-10 bg-pink-400 rounded-md font-outfit text-xl text-secondary font-semibold text-center" 
+                style={{marginInline: value}}>2</div>
+                <div className="w-full h-10 bg-pink-200 rounded-md font-outfit text-xl text-secondary font-semibold text-center">3</div>
+            </div>
+            <InheritTemplate 
+                inheritValue={inheritValue}
+                property={{name: name, value: value}}
+            />
+            <CopyAndPasteTemplate 
+                name={name}
+                value={value}
+            />
+        </div>
+    )
+}
