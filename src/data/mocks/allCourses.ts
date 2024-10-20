08049bcd3314b223793d9a4be847177d8b16fb59
Rapha Developer <contatosraphaelmoreira@gmail.com>
2024-10-20 15:28:32 -0300

    feat: export 12 full courses: ...start with a, b, c, d, e, f, g, h, i, j, l, m

---
+++ src/data/mocks/allCourses.ts
@@ -0,0 +1,55 @@
+import { CourseProps } from "../../types/CourseProps";
+import { propertiesThatStartWithA } from "./courses/propertiesThatStartWithA";
+import { propertiesThatStartWithB } from "./courses/propertiesThatStartWithB";
+import { propertiesThatStartWithC } from "./courses/propertiesThatStartWithC";
+import { propertiesThatStartWithD } from "./courses/propertiesThatStartWithD";
+import { propertiesThatStartWithE } from "./courses/propertiesThatStartWithE";
+import { propertiesThatStartWithF } from "./courses/propertiesThatStartWithF";
+import { propertiesThatStartWithG } from "./courses/propertiesThatStartWithG";
+import { propertiesThatStartWithH } from "./courses/propertiesThatStartWithH";
+import { propertiesThatStartWithI } from "./courses/propertiesThatStartWithI";
+import { propertiesThatStartWithJ } from "./courses/propertiesThatStartWithJ";
+import { propertiesThatStartWithL } from "./courses/propertiesThatStartWithL";
+import { propertiesThatStartWithM } from "./courses/propertiesThatStartWithM";
+
+type allCoursesProps = {
+    course: CourseProps
+}
+export const allCourses: allCoursesProps[]  = [
+    {
+        course: propertiesThatStartWithA,
+    },
+    {
+        course: propertiesThatStartWithB,
+    },
+    {
+        course: propertiesThatStartWithC,
+    },
+    {
+        course: propertiesThatStartWithD,
+    },
+    {
+        course: propertiesThatStartWithE,
+    },
+    {
+        course: propertiesThatStartWithF,
+    },
+    {
+        course: propertiesThatStartWithG,
+    },
+    {
+        course: propertiesThatStartWithH,
+    },
+    {
+        course: propertiesThatStartWithI,
+    },
+    {
+        course: propertiesThatStartWithJ,
+    },
+    {
+        course: propertiesThatStartWithL,
+    },
+    {
+        course: propertiesThatStartWithM,
+    },
+]
