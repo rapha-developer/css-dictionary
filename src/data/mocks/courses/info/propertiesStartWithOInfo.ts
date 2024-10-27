29ded83e4af4b06fcbda49b36b46d26c90e7326c
Rapha Developer <contatosraphaelmoreira@gmail.com>
2024-10-27 15:44:49 -0300

    feat: export (properties start with O course info) data constant. Responsible to generate information about this course

---
+++ src/data/mocks/courses/info/propertiesStartWithOInfo.ts
@@ -0,0 +1,10 @@
+import { CourseInfoProps } from "../../../../types/CourseProps";
+
+export const propertiesStartWithOCourseInfo: CourseInfoProps = {
+    resume: "Neste curso, você aprenderá 11 propriedades css que começa com a letra O através de vários exemplos. Aumente seus conhecimentos na área de desenvolvimento web dentro da linguagem css",
+    body: {
+        first: "Dentre as propriedades que você aprenderá estão: object-fit, object-position, offset-position, opacity, order, outline, outline-offset, overflow, overflow-anchor, overflow-wrap, overflow-x & overflow-y.",
+        second: "Cada propriedade css contém um nome, uma nítida e detalhada definição do que faz, como também os valores de entrada formalmente aceitos pela linguagem de estilização CSS. Além de númerosos exemplos que buscam esclarecer o que cada valor realmente executa dentro de uma aplicação web."
+    },
+    curriculumDesc: "Esse curso foi construído com o intuito de entender minunciosamente cada propriedade css que começa com letra O. As propriedades, quando visualizadas com atenção, podem auxiliar desenvolvedores na resolução de desafios com a linguagem css.",
+}
