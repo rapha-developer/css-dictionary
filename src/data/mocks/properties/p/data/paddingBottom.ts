1328dad4fe8a649dfec52ad7987b97941e3fea6a
Rapha Developer <contatosraphaelmoreira@gmail.com>
2024-10-30 11:08:23 -0300

    feat: export padding-bottom constant (with all information about property). Export padding-bottom template for padding-bottom examples

---
+++ src/data/mocks/properties/p/data/paddingBottom.ts
@@ -0,0 +1,52 @@
+import { PropertyProps } from "../../../../../types/PropertyProps";
+import { genericPropertyExamplesData, genericPropertyValuesData } from "../../../GenericPropertyAtributes";
+
+export const paddingBottomData: PropertyProps = {
+    name: "padding bottom",
+    definition: "A propriedade padding-bottom define o preenchimento inferior (espaço) de um elemento. Observação: valores negativos não são permitidos.",
+    category: "css property",
+    values: [
+        {
+            name: "length",
+            value: "Especifica um preenchimento inferior fixo em px, pt, cm, etc. O valor padrão é 0. Leia sobre unidades de comprimento no glossário."
+        },
+        {
+            name: "%",
+            value: "Especifica um preenchimento inferior em porcentagem da largura do elemento que o contém"
+        },
+        genericPropertyValuesData.initial,
+        genericPropertyValuesData.inherit
+    ],
+    examples: [
+        {
+            name: "0",
+            code: "0"
+        },
+        {
+            name: "10px",
+            code: "10px"
+        },
+        {
+            name: "25px",
+            code: "25px"
+        },
+        {
+            name: "50px",
+            code: "50px"
+        },
+        {
+            name: "1%",
+            code: "1%"
+        },
+        {
+            name: "5%",
+            code: "5%"
+        },
+        {
+            name: "10%",
+            code: "10%"
+        },
+        genericPropertyExamplesData.initial,
+        genericPropertyExamplesData.inherit
+    ]
+}
