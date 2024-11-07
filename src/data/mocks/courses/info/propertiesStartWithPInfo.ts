96bfb10924f53537d41c4f8bbf7426a877f9379f
Rapha Developer <contatosraphaelmoreira@gmail.com>
2024-11-07 11:14:42 -0300

    feat: export (properties start with P course info) data constant. Responsible to generate information about this course

---
+++ src/data/mocks/courses/info/propertiesStartWithPInfo.ts
@@ -0,0 +1,10 @@
+import { CourseInfoProps } from "../../../../types/CourseProps";
+
+export const propertiesStartWithPCourseInfo: CourseInfoProps = {
+    resume: "Neste curso, você aprenderá 18 propriedades css que começa com a letra P através de vários exemplos. Aumente seus conhecimentos na área de desenvolvimento web dentro da linguagem css",
+    body: {
+        first: "Dentre as propriedades que você aprenderá estão: padding, padding-block, padding-bottom, padding-inline, padding-left, padding-right, padding-top, perspective, place-content, place-items, place-self, pointer-events, position & entre outros.",
+        second: "Cada propriedade css contém um nome, uma nítida e detalhada definição do que faz, como também os valores de entrada formalmente aceitos pela linguagem de estilização CSS. Além de númerosos exemplos que buscam esclarecer o que cada valor realmente executa dentro de uma aplicação web."
+    },
+    curriculumDesc: "Esse curso foi construído com o intuito de entender minunciosamente cada propriedade css que começa com letra P. As propriedades, quando visualizadas com atenção, podem auxiliar desenvolvedores na resolução de desafios com a linguagem css.",
+}
