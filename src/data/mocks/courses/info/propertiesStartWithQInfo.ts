3906dc57af302a079f2ba8b55900cad09e0b01d4
Rapha Developer <contatosraphaelmoreira@gmail.com>
2024-11-07 11:46:58 -0300

    feat: export (properties start with Q course info) data constant. Responsible to generate information about this course

---
+++ src/data/mocks/courses/info/propertiesStartWithQInfo.ts
@@ -0,0 +1,10 @@
+import { CourseInfoProps } from "../../../../types/CourseProps";
+
+export const propertiesStartWithQCourseInfo: CourseInfoProps = {
+    resume: "Neste curso, você aprenderá 1 propriedade css que começa com a letra Q através de vários exemplos. Aumente seus conhecimentos na área de desenvolvimento web dentro da linguagem css",
+    body: {
+        first: "A propriedade que você aprenderá será quotes.",
+        second: "Cada propriedade css contém um nome, uma nítida e detalhada definição do que faz, como também os valores de entrada formalmente aceitos pela linguagem de estilização CSS. Além de númerosos exemplos que buscam esclarecer o que cada valor realmente executa dentro de uma aplicação web."
+    },
+    curriculumDesc: "Esse curso foi construído com o intuito de entender minunciosamente cada propriedade css que começa com letra Q. As propriedades, quando visualizadas com atenção, podem auxiliar desenvolvedores na resolução de desafios com a linguagem css.",
+}
