9b2b484bf9df430edb10ffbeb62ca2bd2b388620
Rapha Developer <contatosraphaelmoreira@gmail.com>
2024-11-27 10:07:25 -0300

    text: change the text to the correct format

--- src/data/mocks/courses/info/propertiesStartWithSInfo.ts
+++ src/data/mocks/courses/info/propertiesStartWithSInfo.ts
@@ -3,7 +3,7 @@ import { CourseInfoProps } from "../../../../types/CourseProps";
 export const propertiesStartWithSCourseInfo: CourseInfoProps = {
     resume: "Neste curso, você aprenderá 3 propriedades css que começam com a letra S através de vários exemplos. Aumente seus conhecimentos na área de desenvolvimento web dentro da linguagem css",
     body: {
-        first: "A propriedade que você aprenderá será scale, scrollbar-behavior e scrollbar-color.",
+        first: "Dentre as propriedades que você aprenderá estão: scale, scrollbar-behavior e scrollbar-color.",
         second: "Cada propriedade css contém um nome, uma nítida e detalhada definição do que faz, como também os valores de entrada formalmente aceitos pela linguagem de estilização CSS. Além de númerosos exemplos que buscam esclarecer o que cada valor realmente executa dentro de uma aplicação web."
     },
     curriculumDesc: "Esse curso foi construído com o intuito de entender minunciosamente cada propriedade css que começa com letra S. As propriedades, quando visualizadas com atenção, podem auxiliar desenvolvedores na resolução de desafios com a linguagem css.",
